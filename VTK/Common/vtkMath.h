d8290d417a7a4079d4ae769bd12242139b121182
schroede <schroede>
1994-02-08 11:48:23 +0000

    *** empty log message ***

--- VTK/Common/vtkMath.h
+++ VTK/Common/vtkMath.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkMath.h,v $
   Language:  C++
-  Date:      $Date: 1994-02-06 08:06:22 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 1994-02-08 11:48:23 $
+  Version:   $Revision: 1.8 $
 
 This file is part of the Visualization Library. No part of this file or its 
 contents may be copied, reproduced or altered in any way without the express
@@ -29,6 +29,10 @@ public:
   float DegreesToRadians() {return 0.018977369;};
   float Dot(float x[3], float y[3]) 
     {return x[0]*y[0] + x[1]*y[1] + x[2]*y[2];};
+  void Cross(float x[3], float y[3], float z[3])
+    {z[0] = x[1]*y[2] - x[2]*y[1]; 
+     z[1] = x[2]*y[0] - x[0]*y[2];
+     z[2] = x[0]*y[1] - x[1]*y[0];};
   float Norm(float x[3])
     {return sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);};
   void RandomSeed(long s);  
