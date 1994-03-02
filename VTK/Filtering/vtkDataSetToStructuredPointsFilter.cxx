55dffdd41751012d57c0b68b92f73fdd8cd7455a
schroede <schroede>
1994-03-02 08:59:12 +0000

    Initial revision

---
+++ VTK/Filtering/vtkDataSetToStructuredPointsFilter.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program:   Visualization Library
+  Module:    $RCSfile: vtkDataSetToStructuredPointsFilter.cxx,v $
+  Language:  C++
+  Date:      $Date: 1994-03-02 08:59:13 $
+  Version:   $Revision: 1.1 $
+
+Description:
+---------------------------------------------------------------------------
+This file is part of the Visualization Library. No part of this file
+or its contents may be copied, reproduced or altered in any way
+without the express written consent of the authors.
+
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
+
+=========================================================================*/
+#include "P2SPtsF.hh"
+
+void vlPolyToStructuredPointsFilter::Update()
+{
+  vlPolyFilter::Update();
+}
+
+void vlPolyToStructuredPointsFilter::PrintSelf(ostream& os, vlIndent indent)
+{
+  if (this->ShouldIPrint(vlPolyToStructuredPointsFilter::GetClassName()))
+    {
+    this->PrintWatchOn(); // watch for multiple inheritance
+    
+    vlStructuredPoints::PrintSelf(os,indent);
+    vlPolyFilter::PrintSelf(os,indent);
+    
+    this->PrintWatchOff(); // stop worrying about it now
+    }
+}
