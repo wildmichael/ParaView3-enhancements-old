f7c5f9504601606446efc8708459e44bec7eedb1
schroede <schroede>
1994-07-11 22:46:32 +0000

    ENH: Added comments.

--- VTK/Common/vtkObject.h
+++ VTK/Common/vtkObject.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkObject.h,v $
   Language:  C++
-  Date:      $Date: 1994-05-08 07:25:53 $
-  Version:   $Revision: 1.12 $
+  Date:      $Date: 1994-07-11 22:46:32 $
+  Version:   $Revision: 1.13 $
 
 This file is part of the Visualization Library. No part of this file or its 
 contents may be copied, reproduced or altered in any way without the express
@@ -13,11 +13,11 @@ written consent of the authors.
 Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
 
 =========================================================================*/
-//
-// Common abstract class for Visualization Library.  Maintains debug
-// flag, reference counting, modified time, and other common
-// functions/parameters.
-//
+// .NAME vlObject - abstract base class for many objects
+// .SECTION Description
+// vlObject is the base class for many objects in the visualization 
+// library. vlObject provides methods for reference counting, keeping 
+// track of modified time, debugging, and printing object.
 
 #ifndef __vlObject_hh
 #define __vlObject_hh
@@ -28,43 +28,42 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 #include "Indent.hh"
 #include "PrintLst.hh"
 
-//
-// Common #defines / parameters
-//
-
-//
-// Class definition
-//
-
 class vlObject 
 {
 public:
   vlObject();
   virtual ~vlObject();
+  virtual char *GetClassName() {return "vlObject";};
+
+  // reference counting
   void Register(vlObject* o);
   void UnRegister(vlObject* o);
   int  GetRefCount() {return this->RefCount;};
+
+  // debugging
   void DebugOn();
   void DebugOff();
-  virtual unsigned long int GetMTime() {return this->MTime.GetMTime();};
   int GetDebug();
-  void Modified() {MTime.Modified();};
-  virtual char *GetClassName() {return "vlObject";};
 
+  // modified time
+  virtual unsigned long int GetMTime() { return this->MTime.GetMTime(); };
+  void Modified() {MTime.Modified();};
+
+  // printing
+  virtual void PrintSelf(ostream& os, vlIndent indent);
   void Print(ostream& os);
   virtual void PrintHeader(ostream& os, vlIndent indent);
-  virtual void PrintSelf(ostream& os, vlIndent indent);
   int ShouldIPrint(char *a) { return this->PrintList.ShouldIPrint(a);};
   virtual void PrintTrailer(ostream& os, vlIndent indent);
   void PrintWatchOn() {this->PrintList.ActiveOn();};
   void PrintWatchOff() {this->PrintList.ActiveOff();};
 
 protected:
-  int Debug;       // Enable debug messages
+  int Debug;         // Enable debug messages
   vlTimeStamp MTime; // Keep track of modification time
 
 private:
-  int RefCount;    // Number of uses of this object by other objects
+  int RefCount;      // Number of uses of this object by other objects
   vlPrintList PrintList;
 
   friend ostream& operator<<(ostream& os, vlObject& o);
