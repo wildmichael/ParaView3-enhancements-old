e53477aca2d264507e404b7000eb20f54426f5fc
schroede <schroede>
1994-08-15 07:45:29 +0000

    ENH: Added documentation.

--- VTK/Common/vtkLookupTable.h
+++ VTK/Common/vtkLookupTable.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkLookupTable.h,v $
   Language:  C++
-  Date:      $Date: 1994-05-15 19:24:26 $
-  Version:   $Revision: 1.9 $
+  Date:      $Date: 1994-08-15 07:45:31 $
+  Version:   $Revision: 1.10 $
 
 This file is part of the Visualization Library. No part of this file or its 
 contents may be copied, reproduced or altered in any way without the express
@@ -13,9 +13,13 @@ written consent of the authors.
 Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
 
 =========================================================================*/
-//
-// LookupTable takes PolyData as input
-//
+// .NAME vlLookupTable - map scalar values into colors; generate color tables
+// .SECTION Description
+// vlLookupTable is an object that is used by mapper objects to map scalar 
+// values into RGB color specification. The color table can be created by 
+// direct insertion of color values, or by specifying  hue, saturation, and 
+// value range and generating a table.
+
 #ifndef __vlLookupTable_h
 #define __vlLookupTable_h
 
@@ -31,6 +35,8 @@ public:
   char *GetClassName() {return "vlLookupTable";};
   void PrintSelf(ostream& os, vlIndent indent);
 
+  // Description:
+  // Set the number of colors in the lookup table.
   vlSetClampMacro(NumberOfColors,int,8, 65536);
   vlGetMacro(NumberOfColors,int);
 
@@ -38,12 +44,18 @@ public:
   void SetTableRange(float min, float max);
   vlGetVectorMacro(TableRange,float);
 
+  // Description:
+  // Set the range in hue (using automatic generation).
   vlSetVector2Macro(HueRange,float);
   vlGetVectorMacro(HueRange,float);
 
+  // Description:
+  // Set the range in saturation (using automatic generation).
   vlSetVector2Macro(SaturationRange,float);
   vlGetVectorMacro(SaturationRange,float);
 
+  // Description:
+  // Set the range in value (using automatic generation).
   vlSetVector2Macro(ValueRange,float);
   vlGetVectorMacro(ValueRange,float);
 
