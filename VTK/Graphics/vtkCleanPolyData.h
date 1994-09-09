4189ecbdf22575d4ccca8f8527a813438268ac4b
schroede <schroede>
1994-09-09 22:30:15 +0000

    ENH: Doc change.

--- VTK/Graphics/vtkCleanPolyData.h
+++ VTK/Graphics/vtkCleanPolyData.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkCleanPolyData.h,v $
   Language:  C++
-  Date:      $Date: 1994-08-11 08:56:02 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 1994-09-09 22:30:15 $
+  Version:   $Revision: 1.4 $
 
 This file is part of the Visualization Library. No part of this file
 or its contents may be copied, reproduced or altered in any way
@@ -15,7 +15,7 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 =========================================================================*/
 // .NAME vlCleanPolyData - merge duplicate points and remove degenerate primitives
 // .SECTION Description
-// vlCleanPolyData is a filter that takes as input polygonal data and 
+// vlCleanPolyData is a filter that takes polygonal data as input and 
 // generates polygonal as output. vlCleanPolyData merges duplicate 
 // points (within specified tolerance) and transforms degenerate 
 // topology into appropriate form (for example, triangle is converted
