b5f186c358b93d34f204c8c3f78b8e759eb6a77b
schroede <schroede>
1994-09-20 22:13:45 +0000

    *** empty log message ***

--- VTK/Graphics/vtkPlaneSource.h
+++ VTK/Graphics/vtkPlaneSource.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkPlaneSource.h,v $
   Language:  C++
-  Date:      $Date: 1994-09-09 10:46:38 $
-  Version:   $Revision: 1.13 $
+  Date:      $Date: 1994-09-20 22:13:45 $
+  Version:   $Revision: 1.14 $
 
 This file is part of the Visualization Library. No part of this file or its 
 contents may be copied, reproduced or altered in any way without the express
@@ -13,7 +13,7 @@ written consent of the authors.
 Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
 
 =========================================================================*/
-// .NAME - create an array of quadrilaterals located in the plane
+// .NAME vlPlaneSource - create an array of quadrilaterals located in the plane
 // .SECTION Description
 // vlPlaneSource creates an m x n array of quadrilaterals arranged as a
 // regular tiling in the plane. The plane is centered at the origin, and 
