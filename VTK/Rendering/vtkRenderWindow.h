cb944a272827914a3f9cbdfe1fc1012cc6e2010c
martink <martink>
1995-01-05 10:57:03 +0000

    added focal depth, anti aliasing, motion blur

--- VTK/Rendering/vtkRenderWindow.h
+++ VTK/Rendering/vtkRenderWindow.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkRenderWindow.h,v $
   Language:  C++
-  Date:      $Date: 1994-12-14 16:34:46 $
-  Version:   $Revision: 1.11 $
+  Date:      $Date: 1995-01-05 10:57:15 $
+  Version:   $Revision: 1.12 $
 
 This file is part of the Visualization Library. No part of this file or its
 contents may be copied, reproduced or altered in any way without the express
@@ -57,6 +57,10 @@ public:
   virtual void Frame() = 0;
 
   // Description:
+  // Performed at the end of the rendering process to generate image.
+  virtual void CopyResultFrame();
+
+  // Description:
   // Create a device specific renderer.
   virtual vlRenderer  *MakeRenderer() = 0;
 
@@ -75,6 +79,10 @@ public:
   // Description:
   // Create a device specific property.
   virtual vlProperty    *MakeProperty() = 0;
+
+  // Description:
+  // Create a device specific texture.
+  virtual vlTexture     *MakeTexture() = 0;
 
   // Description:
   // Create an interactor to control renderers in this window.
@@ -154,7 +162,26 @@ public:
   virtual unsigned char *GetPixelData(int x,int y,int x2,int y2) = 0;
   virtual void SetPixelData(int x,int y,int x2,int y2,unsigned char *) = 0;
 
+  // Description:
+  // Set the number of frames for doing anti aliasing, default is zero.
+  vlGetMacro(AAFrames,int);
+  vlSetMacro(AAFrames,int);
+
+  // Description:
+  // Set the number of frames for doing focal depth, default is zero.
+  vlGetMacro(FDFrames,int);
+  vlSetMacro(FDFrames,int);
+
+  // Description:
+  // Set the number of sub frames for doing motion blur.
+  vlGetMacro(SubFrames,int);
+  vlSetMacro(SubFrames,int);
+
 protected:
+  virtual void DoStereoRender();
+  virtual void DoFDRender();
+  virtual void DoAARender();
+
   vlRendererCollection Renderers;
   char Name[80];
   int Size[2];
@@ -170,6 +197,16 @@ protected:
   vlRenderWindowInteractor *Interactor;
   char *FileName;
   unsigned char* temp_buffer;  // used for red blue stereo
+  unsigned char** AABuffer;    // used for anti aliasing
+  int AAFrames;
+  unsigned char** FDBuffer;    // used for focal depth
+  int FDFrames;
+  unsigned char** SubBuffer;   // used for sub frames
+  int SubFrames;               // number of sub frames
+  int CurrentSubFrame;         // what one are we on
+  unsigned char* ResultFrame;  // used for any non immediate rendering
 };
 
 #endif
+
+
