b77a0efbfb2dd7ba6b5b948bc05afe26eff30a4d
schroede <schroede>
1995-02-15 09:27:22 +0000

    Initial revision

---
+++ VTK/Filtering/vtkUnstructuredGridSource.h
@@ -0,0 +1,49 @@
+/*=========================================================================
+
+  Program:   Visualization Library
+  Module:    $RCSfile: vtkUnstructuredGridSource.h,v $
+  Language:  C++
+  Date:      $Date: 1995-02-15 09:27:23 $
+  Version:   $Revision: 1.1 $
+
+This file is part of the Visualization Library. No part of this file
+or its contents may be copied, reproduced or altered in any way
+without the express written consent of the authors.
+
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
+
+=========================================================================*/
+// .NAME vlUnstructuredGridSource - abstract class whose subclasses generate unstructured grid data
+// .SECTION Description
+// vlUnstructuredGridSource is an abstract class whose subclasses generate unstructured grid data.
+
+#ifndef __vlUnstructuredGridSource_h
+#define __vlUnstructuredGridSource_h
+
+#include "Source.hh"
+#include "UGrid.hh"
+
+class vlUnstructuredGridSource : public vlSource, public vlUnstructuredGrid 
+{
+public:
+  char *GetClassName() {return "vlUnstructuredGridSource";};
+  void PrintSelf(ostream& os, vlIndent indent);
+
+  // Object interface
+  void Modified();
+  unsigned long int GetMTime();
+  void DebugOn();
+  void DebugOff();
+
+  //DataSet interface
+  void Update();
+
+protected:
+  //Source interface
+  int GetDataReleased();
+  void SetDataReleased(int flag);
+};
+
+#endif
+
+
