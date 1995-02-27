e562fd3c9317fb43ac563de86b55850040f97c59
schroede <schroede>
1995-02-27 13:17:46 +0000

    ENH: Got it working and tested.

--- VTK/Graphics/vtkEdgePoints.h
+++ VTK/Graphics/vtkEdgePoints.h
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkEdgePoints.h,v $
   Language:  C++
-  Date:      $Date: 1995-02-26 10:26:54 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 1995-02-27 13:17:46 $
+  Version:   $Revision: 1.2 $
 
 This file is part of the Visualization Library. No part of this file
 or its contents may be copied, reproduced or altered in any way
@@ -16,18 +16,19 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 // .NAME vlEdgePoints - generate points on iso-surface
 // .SECTION Description
 // vlEdgePoints is a filter that takes as input any dataset and 
-// generates on output points that lie on an iso-surface. The points are
-// created by interpolation along cells whose end-points are below and 
-// above the contour value.
-//    vlEdgePoints can be considered a "poor man's" dividing cubes algorithm.
-// Points are generated only on the edges of cells, not in the interior.
+// generates for output a set of points that lie on an iso-surface. The 
+// points are created by interpolation along cells edges whose end-points are 
+// below and above the contour value.
+// .SECTION Caveats
+// vlEdgePoints can be considered a "poor man's" dividing cubes algorithm
+// (see vlDividingCubes). Points are generated only on the edges of cells, 
+// not in the interior, and at lower density than dividing cubes. However, it 
+// is more general than dividing cubes since it treats any type of dataset.
 
 #ifndef __vlEdgePoints_h
 #define __vlEdgePoints_h
 
 #include "DS2PolyF.hh"
-
-#define MAX_CONTOURS 256
 
 class vlEdgePoints : public vlDataSetToPolyFilter
 {
