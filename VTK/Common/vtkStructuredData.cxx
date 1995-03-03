34ebb40ecfa1135d5e9a863dee33527a7e1f8703
schroede <schroede>
1995-03-03 17:12:32 +0000

    *** empty log message ***

--- VTK/Common/vtkStructuredData.cxx
+++ VTK/Common/vtkStructuredData.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkStructuredData.cxx,v $
   Language:  C++
-  Date:      $Date: 1995-02-28 22:41:08 $
-  Version:   $Revision: 1.20 $
+  Date:      $Date: 1995-03-03 17:12:32 $
+  Version:   $Revision: 1.21 $
 
 This file is part of the Visualization Library. No part of this file
 or its contents may be copied, reproduced or altered in any way
@@ -288,7 +288,7 @@ void vlStructuredData::_GetPointCells(int ptId, vlIdList& cellIds)
 {
   int ptDim[3], cellDim[3];
   int ptLoc[3], cellLoc[3];
-  int i, j, cellId, add;
+  int i, j, cellId;
   static int offset[8][3] = {{-1,0,0}, {-1,-1,0}, {-1,-1,-1}, {-1,0,-1},
                              {0,0,0},  {0,-1,0},  {0,-1,-1},  {0,0,-1}};
 
@@ -311,16 +311,13 @@ void vlStructuredData::_GetPointCells(int ptId, vlIdList& cellIds)
 
   for (j=0; j<8; j++) 
     {
-    for (add=1, i=0; i<3; i++) 
+    for (i=0; i<3; i++) 
       {
       cellLoc[i] = ptLoc[i] + offset[j][i];
       if ( cellLoc[i] < 0 || cellLoc[i] >= cellDim[i] ) 
-        {
-        add = 0;
         break;
-        }
       }
-    if ( add ) 
+    if ( i >= 3 ) //add cell
       {
       cellId = cellLoc[0] + cellLoc[1]*cellDim[0] + 
                             cellLoc[2]*cellDim[0]*cellDim[1];
