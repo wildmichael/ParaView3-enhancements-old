e1b6fad1ec91df0bfa7163138d1c3ab27627dc7e
schroede <schroede>
1995-06-08 20:24:08 +0000

    ENH: Finale implementation.

--- VTK/Filtering/vtkCone.cxx
+++ VTK/Filtering/vtkCone.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Library
   Module:    $RCSfile: vtkCone.cxx,v $
   Language:  C++
-  Date:      $Date: 1995-06-08 20:06:12 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 1995-06-08 20:24:08 $
+  Version:   $Revision: 1.5 $
 
 This file is part of the Visualization Library. No part of this file
 or its contents may be copied, reproduced or altered in any way
@@ -14,6 +14,7 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 
 =========================================================================*/
 #include "Cone.hh"
+#include "vlMath.hh"
 
 // Description
 // Construct cone with angle of 45 degrees.
@@ -26,13 +27,20 @@ vlCone::vlCone()
 // Evaluate cone equation.
 float vlCone::EvaluateFunction(float x[3])
 {
-  return 0;
+  static vlMath math;
+  float tanTheta = (float) tan((double)this->Angle*math.DegreesToRadians());
+  return x[0]*x[0] + x[1]*x[1] - x[2]*tanTheta;
 }
 
 // Description
 // Evaluate cone normal.
 void vlCone::EvaluateGradient(float x[3], float g[3])
 {
+  static vlMath math;
+  float tanTheta = (float) tan((double)this->Angle*math.DegreesToRadians());
+  g[0] = 2.0*x[0];
+  g[1] = 2.0*x[1];
+  g[2] = -tanTheta;
 }
 
 void vlCone::PrintSelf(ostream& os, vlIndent indent)
