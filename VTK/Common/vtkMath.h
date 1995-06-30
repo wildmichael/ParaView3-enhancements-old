3c17ccffb72691f107ced1d9fde3650db8a51db7
martink <martink>
1995-06-30 16:30:08 +0000

    more vtk changes

--- VTK/Common/vtkMath.h
+++ VTK/Common/vtkMath.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkMath.h,v $
   Language:  C++
-  Date:      $Date: 1995-06-29 17:55:35 $
-  Version:   $Revision: 1.18 $
+  Date:      $Date: 1995-06-30 16:30:08 $
+  Version:   $Revision: 1.19 $
 
 This file is part of the Visualization Toolkit. No part of this file or its 
 contents may be copied, reproduced or altered in any way without the express
