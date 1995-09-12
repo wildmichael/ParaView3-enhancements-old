de77092673e195f9ce9c1d6c1104c18c94a27ece
lorensen <lorensen>
1995-09-12 12:34:21 +0000

    ERR: spelling errors.

--- VTK/Rendering/vtkPicker.h
+++ VTK/Rendering/vtkPicker.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkPicker.h,v $
   Language:  C++
-  Date:      $Date: 1995-08-31 21:20:58 $
-  Version:   $Revision: 1.11 $
+  Date:      $Date: 1995-09-12 12:34:21 $
+  Version:   $Revision: 1.12 $
 
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -46,11 +46,11 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 // the camera's position.
 //
 // vtkPicker may return more than one actor, since more than one bounding 
-// box may be intersected. VlPicker returns the list of actors that were hit, 
+// box may be intersected. vtkPicker returns the list of actors that were hit, 
 // the pick coordinates in world and untransformed mapper space, and the 
 // actor and mapper that are "closest" to the camera. The closest actor is 
 // the one whose center point (i.e., center of bounding box) projected on the
-// the ray is closest to the camera.
+// ray is closest to the camera.
 // .SECTION Caveats
 // vtkPicker and its subclasses will not pick actors that are "unpickable" 
 // (see vtkActor) or are fully transparent.
