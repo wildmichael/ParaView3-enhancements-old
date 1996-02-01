fef0fcad60b40f7bbe76279ee35a6de0e2f4037e
martink <martink>
1996-02-01 14:23:51 +0000

    using streams now

--- VTK/IO/vtkUnstructuredGridReader.cxx
+++ VTK/IO/vtkUnstructuredGridReader.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkUnstructuredGridReader.cxx,v $
   Language:  C++
-  Date:      $Date: 1996-01-25 13:26:14 $
-  Version:   $Revision: 1.22 $
+  Date:      $Date: 1996-02-01 14:23:52 $
+  Version:   $Revision: 1.23 $
 
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -227,9 +227,10 @@ void vtkUnstructuredGridReader::Execute()
         types = new int[ncells];
         if (this->Reader.GetFileType() == VTK_BINARY)
           {
-          if ( (fgets(line,256,this->Reader.GetFP()) == NULL) ||
-	       (fread(types,sizeof(int),ncells,this->Reader.GetFP()) 
-		!= ncells) )
+	  // suck up newline
+	  this->Reader.GetIStream()->getline(line,256);
+	  this->Reader.GetIStream()->read(types,sizeof(int)*ncells);
+	  if (this->Reader.GetIStream()->eof())
             {
             vtkErrorMacro(<<"Error reading binary cell types!");
             return;
