aa613a0a91eaa16f7d2833860984fb987ea53684
schroede <schroede>
1996-03-05 21:17:19 +0000

    ENH: Added barycentric coordinate computation.

--- VTK/Common/vtkTriangle.h
+++ VTK/Common/vtkTriangle.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkTriangle.h,v $
   Language:  C++
-  Date:      $Date: 1996-01-09 21:49:53 $
-  Version:   $Revision: 1.25 $
+  Date:      $Date: 1996-03-05 21:17:19 $
+  Version:   $Revision: 1.26 $
 
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -85,6 +85,8 @@ public:
   void TriangleCenter(float p1[3], float p2[3], float p3[3], float center[3]);
   float TriangleArea(float p1[3], float p2[3], float p3[3]);
   float Circumcircle(float  p1[2], float p2[2], float p3[2], float center[2]);
+  int BarycentricCoords(float x[2], float  x1[2], float x2[2], float x3[2], 
+                        float bcoords[3]);
 };
 
 // Description:
