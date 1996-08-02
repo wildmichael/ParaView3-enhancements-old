b6447434a75fd66cf204ebf9d4266a28183006ae
martink <martink>
1996-08-02 20:19:53 +0000

    vtk1.2 copyright changes

--- VTK/Hybrid/vtkImplicitModeller.cxx
+++ VTK/Hybrid/vtkImplicitModeller.cxx
@@ -3,11 +3,11 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImplicitModeller.cxx,v $
   Language:  C++
-  Date:      $Date: 1996-07-19 13:05:10 $
-  Version:   $Revision: 1.32 $
+  Date:      $Date: 1996-08-02 20:19:53 $
+  Version:   $Revision: 1.33 $
 
 
-Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
+Copyright (c) 1993-1996 Ken Martin, Will Schroeder, Bill Lorensen.
 
 This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
 The following terms apply to all files associated with the software unless
