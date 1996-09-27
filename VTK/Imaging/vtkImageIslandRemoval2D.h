f249e77ccc159166c6e5d31450f94b488cd7f589
lawcc <lawcc>
1996-09-27 12:40:17 +0000

    Some name changes.

--- VTK/Imaging/vtkImageIslandRemoval2D.h
+++ VTK/Imaging/vtkImageIslandRemoval2D.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageIslandRemoval2D.h,v $
   Language:  C++
-  Date:      $Date: 1996-09-26 21:07:36 $
-  Version:   $Revision: 1.6 $
+  Date:      $Date: 1996-09-27 12:40:25 $
+  Version:   $Revision: 1.7 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -38,15 +38,15 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 
 =========================================================================*/
-// .NAME vtkImageIslandRemoval2d - Removes small clusters in masks.
+// .NAME vtkImageIslandRemoval2D - Removes small clusters in masks.
 // .SECTION Description
-// vtkImageIslandRemoval2d computes the area of separate islands in 
+// vtkImageIslandRemoval2D computes the area of separate islands in 
 // a mask image.  It removes any island that has less than AreaThreshold
 // pixels.  Output has the same ScalarType as input.
 
 
-#ifndef __vtkImageIslandRemoval2d_h
-#define __vtkImageIslandRemoval2d_h
+#ifndef __vtkImageIslandRemoval2D_h
+#define __vtkImageIslandRemoval2D_h
 
 
 #include "vtkImageFilter.h"
@@ -58,15 +58,15 @@ typedef struct{
   void *outPtr;
   int idx0;
   int idx1;
-  } vtkImage2dIslandPixel;
+  } vtkImage2DIslandPixel;
 
 
 
-class vtkImageIslandRemoval2d : public vtkImageFilter
+class vtkImageIslandRemoval2D : public vtkImageFilter
 {
 public:
-  vtkImageIslandRemoval2d();
-  char *GetClassName() {return "vtkImageIslandRemoval2d";};
+  vtkImageIslandRemoval2D();
+  char *GetClassName() {return "vtkImageIslandRemoval2D";};
   void PrintSelf(ostream& os, vtkIndent indent);
   
   void InterceptCacheUpdate(vtkImageRegion *region);
