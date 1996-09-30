58613bc69c6eecce863c37e2db003eb3b7f88e29
schroede <schroede>
1996-09-30 16:34:35 +0000

    ENH: Modifed comment

--- VTK/Common/vtkCell.h
+++ VTK/Common/vtkCell.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkCell.h,v $
   Language:  C++
-  Date:      $Date: 1996-09-30 16:29:52 $
-  Version:   $Revision: 1.34 $
+  Date:      $Date: 1996-09-30 16:34:35 $
+  Version:   $Revision: 1.35 $
 
 
 Copyright (c) 1993-1996 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -172,10 +172,9 @@ public:
   // Description:
   // Cut (or clip) the cell based on the input cellScalars and the
   // specified value. The output of the clip operation will be one or
-  // more simplices (i.e., vertices, lines, triangles, or tetrahedron)
-  // of the same topological dimension as the cell. The flag insideOut
-  // controls what part of the cell is considered inside - normally
-  // cell points whose scalar value is greater than "value" are
+  // more cells of the same topological dimension as the original cell. 
+  // The flag insideOut controls what part of the cell is considered inside - 
+  // normally cell points whose scalar value is greater than "value" are
   // considered inside. If insideOut is on, this is reversed.
   virtual void Clip(float value, vtkFloatScalars *cellScalars, 
                     vtkPointLocator *locator, vtkCellArray *connectivity,
