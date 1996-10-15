376268b9f4eda2ea542759b7b7f82b9be69c6491
schroede <schroede>
1996-10-15 12:49:59 +0000

    ERR: Made the algorithm more robust.

--- VTK/Graphics/vtkDelaunay2D.h
+++ VTK/Graphics/vtkDelaunay2D.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkDelaunay2D.h,v $
   Language:  C++
-  Date:      $Date: 1996-08-21 20:52:02 $
-  Version:   $Revision: 1.5 $
+  Date:      $Date: 1996-10-15 12:50:02 $
+  Version:   $Revision: 1.6 $
 
 
 Copyright (c) 1993-1996 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -67,6 +67,15 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 // output). (If you desire to triangulate in a different plane, you'll
 // have to use the vtkTransformFilter to transform the points into and
 // out of the x-y plane.)
+// 
+// The Delaunay triangulation can be numerically sensitive in some cases. To
+// prevent problems, try to avoid injecting points that will result in
+// triangles with bad aspect ratios (1000:1 or greater). In practice this
+// means inserting points that are "widely dispersed", and enables smooth
+// transition of triangle sizes throughout the mesh. (You may even want to
+// add extra points to create a better point distribution.) If numerical
+// problems are present, you will see a warning message to this effect at
+// the end of the triangulation process.
 
 // .SECTION Caveats
 // Points arranged on a regular lattice (termed degenerate cases) can be 
