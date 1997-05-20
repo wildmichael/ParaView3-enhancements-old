e740ae8eb1bf5af89eadaeb0fcf8e4dbd4feb045
lawcc <lawcc>
1997-05-20 21:25:08 +0000

    Finally fixed the reader swap bytes bug.

--- VTK/IO/vtkImageReader.h
+++ VTK/IO/vtkImageReader.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageReader.h,v $
   Language:  C++
-  Date:      $Date: 1997-05-14 20:00:39 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 1997-05-20 21:25:09 $
+  Version:   $Revision: 1.8 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -141,6 +141,14 @@ public:
   vtkGetMacro(SwapBytes,int);
   vtkBooleanMacro(SwapBytes,int);
 
+  // Description:
+  // These allow the the min and the max of the extent to be switched.
+  // Because the aspect ratio cannot be negative, a new output 
+  // origin is computed.
+  void SetFlips(int num, int *flip);
+  vtkImageSetMacro(Flips, int);
+  void GetFlips(int num, int *flip);
+  vtkImageGetMacro(Flips, int);
   
   // following should only be used by methods or template helpers, not users
   int DataScalarType;
@@ -148,19 +156,21 @@ public:
   int FileSize;
   int HeaderSize;
   // For seeking to the correct location in the files.
+  // (should really be called DataIncrements instead of FileIncrements ...)
   int FileIncrements[VTK_IMAGE_DIMENSIONS];
   int FileExtent[VTK_IMAGE_EXTENT_DIMENSIONS];
   unsigned short PixelMask;  // Mask each pixel with ...
   int SwapBytes;
-  void Swap(unsigned char *buf, int length);
-  
+  void Swap(unsigned char *buf, int numPixels, int pixelSize);
+  // Flips are flags for each axis specifying whether "reflect" the data.
+  int Flips[VTK_IMAGE_DIMENSIONS];
+  int PixelSize;
   
 protected:
   int ManualHeaderSize;
   int Initialized;
   char *FileName;
-  int PixelSize;
-  
+
   int DataDimensions[VTK_IMAGE_DIMENSIONS];
   float DataAspectRatio[VTK_IMAGE_DIMENSIONS];
   float DataOrigin[VTK_IMAGE_DIMENSIONS];
