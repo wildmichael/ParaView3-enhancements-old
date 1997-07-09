74090083d481356a075d9813310af0922206a3e1
martink <martink>
1997-07-09 20:49:00 +0000

    copyright update to 1998

--- VTK/Filtering/vtkUnstructuredGridSource.cxx
+++ VTK/Filtering/vtkUnstructuredGridSource.cxx
@@ -3,11 +3,11 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkUnstructuredGridSource.cxx,v $
   Language:  C++
-  Date:      $Date: 1997-05-15 23:24:05 $
-  Version:   $Revision: 1.10 $
+  Date:      $Date: 1997-07-09 20:49:00 $
+  Version:   $Revision: 1.11 $
 
 
-Copyright (c) 1993-1996 Ken Martin, Will Schroeder, Bill Lorensen.
+Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
 
 This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
 The following terms apply to all files associated with the software unless
