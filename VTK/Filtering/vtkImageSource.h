be20ef77da2796b5c33a715e756e2466b50cb67d
lorensen <lorensen>
1997-12-11 11:54:54 +0000

    ENH: Added ProgressMethod and Progress ivar

--- VTK/Filtering/vtkImageSource.h
+++ VTK/Filtering/vtkImageSource.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageSource.h,v $
   Language:  C++
-  Date:      $Date: 1997-08-11 18:27:40 $
-  Version:   $Revision: 1.20 $
+  Date:      $Date: 1997-12-11 11:54:55 $
+  Version:   $Revision: 1.21 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -91,11 +91,30 @@ public:
   void SetOutputScalarType(int type);
   int  GetOutputScalarType();
   
+  // Description:
+  // Set/Get the AbortExecute flag for the filter. It's up to the filter writer
+  // to handle premature ending of a filter
+  vtkSetMacro(AbortExecute,int);
+  vtkGetMacro(AbortExecute,int);
+  vtkBooleanMacro(AbortExecute,int);
+
+  // Description:
+  // Specify progress of a filter.
+  vtkSetClampMacro(Progress,float,0.0,1.0);
+  vtkGetMacro(Progress,float);
+
   virtual void SetStartMethod(void (*f)(void *), void *arg);
+  virtual void SetProgressMethod(void (*f)(void *), void *arg);
   virtual void SetEndMethod(void (*f)(void *), void *arg);
   virtual void SetStartMethodArgDelete(void (*f)(void *));
+  virtual void SetProgressMethodArgDelete(void (*f)(void *));
   virtual void SetEndMethodArgDelete(void (*f)(void *));
   
+  // Description:
+  // Update the progress of a filter. If a ProgressMEthod, exists, executes it. Then sets
+  // the Progress ivar to amount.
+  void UpdateProgress(float amount);
+
 protected:
   vtkImageCache *Output;
   // The number of dimensions expected/handled by the execute method
@@ -110,9 +129,14 @@ protected:
   void (*StartMethod)(void *);
   void (*StartMethodArgDelete)(void *);
   void *StartMethodArg;
+  void (*ProgressMethod)(void *);
+  void *ProgressMethodArg;
+  void (*ProgressMethodArgDelete)(void *);
   void (*EndMethod)(void *);
   void (*EndMethodArgDelete)(void *);
   void *EndMethodArg;
+  float Progress;
+  int AbortExecute;
   
   virtual void RecursiveLoopUpdate(int dim, vtkImageRegion *region); 
   virtual void Execute(vtkImageRegion *region); 
