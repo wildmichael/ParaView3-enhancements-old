3b4b06c74d0ee36117668eaa65ab1ac37a81eead
lawcc <lawcc>
1998-06-02 19:44:18 +0000

    Fixed memory leaks

---
+++ VTK/Rendering/vtkLightCollection.cxx
@@ -0,0 +1,95 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkLightCollection.cxx,v $
+  Language:  C++
+  Date:      $Date: 1998-06-02 19:44:18 $
+  Version:   $Revision: 1.1 $
+
+
+Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+#include <stdlib.h>
+#include <math.h>
+
+#include "vtkLightCollection.h"
+
+vtkLightCollection::~vtkLightCollection()
+{
+  this->RemoveAllItems();
+}
+
+// Description:
+// Add a light to the list.
+void vtkLightCollection::AddItem(vtkLight *a) 
+{
+  a->Register(this);
+  this->vtkCollection::AddItem((vtkObject *)a);
+}
+
+// Description:
+// Remove a light from the list.
+void vtkLightCollection::RemoveItem(vtkLight *a) 
+{
+  this->vtkCollection::RemoveItem((vtkObject *)a);
+}
+
+// Description:
+// Determine whether a particular light is present. Returns its position
+// in the list.
+int vtkLightCollection::IsItemPresent(vtkLight *a) 
+{
+  return this->vtkCollection::IsItemPresent((vtkObject *)a);
+}
+
+// Description:
+// Get the next light in the list. NULL is returned when the collection is 
+// exhausted.
+vtkLight *vtkLightCollection::GetNextItem() 
+{ 
+  return (vtkLight *)(this->GetNextItemAsObject());
+}
+
+// Description:
+// protected function to delete an element. Internal use only.
+void vtkLightCollection::DeleteElement(vtkCollectionElement *e)
+{
+  ((vtkLight *)(e->Item))->UnRegister(this); 
+  vtkCollection::DeleteElement(e);
+}
+
+
+
+
+
+
+
