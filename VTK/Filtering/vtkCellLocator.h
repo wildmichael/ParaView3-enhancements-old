c635198f8803e24ad031f3b45daba58768dbbf56
millerjv <millerjv>
1998-10-05 14:52:34 +0000

    FIX: Added a version of IntersectWithLine which returns the CellId of cell intersected.  Fixed a bug where the first octant intersected is on the top, right, back boundary of the bounds. We were getting 0 intersections if the line intersected the top, right or back boundary first (said another way, you could get an intersection if pt1 and pt2 were specified in one order and NO intersections if pt1 and pt2 were flipped).

--- VTK/Filtering/vtkCellLocator.h
+++ VTK/Filtering/vtkCellLocator.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkCellLocator.h,v $
   Language:  C++
-  Date:      $Date: 1998-09-14 13:21:28 $
-  Version:   $Revision: 1.27 $
+  Date:      $Date: 1998-10-05 14:52:34 $
+  Version:   $Revision: 1.28 $
 
 
 Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -66,9 +66,9 @@ class VTK_EXPORT vtkCellLocator : public vtkLocator
 {
 public:
 
-// Description:
-// Construct with automatic computation of divisions, averaging
-// 25 cells per bucket.
+  // Description:
+  // Construct with automatic computation of divisions, averaging
+  // 25 cells per bucket.
   vtkCellLocator();
 
   ~vtkCellLocator();
@@ -83,18 +83,25 @@ public:
 
   // methods that all cell locators must provide
 
-// Description:
-// Return intersection point (if any) of finite line with cells contained
-// in cell locator.
+  // Description:
+  // Return intersection point (if any) of finite line with cells contained
+  // in cell locator.
   virtual int IntersectWithLine(float a0[3], float a1[3], float tol,
 				float& t, float x[3], float pcoords[3],
 				int &subId);
 
+
+  // Return intersection point (if any) AND the cell which was intersected by
+  // the finite line.
+  virtual int IntersectWithLine(float a0[3], float a1[3], float tol,
+				float& t, float x[3], float pcoords[3],
+				int &subId, int &cellId);
+
   virtual vtkIdList *GetCells(int bucket);
 
-// Description:
-// Intersect against another vtkCellLocator returning cells that lie in 
-// intersecting octants. Not implimented yet.
+  // Description:
+  // Intersect against another vtkCellLocator returning cells that lie in 
+  // intersecting octants. Not implimented yet.
   virtual void InitializeIntersection(vtkCellLocator& locator);
 
   virtual int GetNextIntersection(int& bucket1, int& bucket2);
