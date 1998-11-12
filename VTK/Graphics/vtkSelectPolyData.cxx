6fdfda32c4233ce3064236942afde48209b3a24c
lorensen <lorensen>
1998-11-12 14:04:08 +0000

    ERR: memory leaks. this->Mesh was used twice without an UnRegister in between uses.

--- VTK/Graphics/vtkSelectPolyData.cxx
+++ VTK/Graphics/vtkSelectPolyData.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkSelectPolyData.cxx,v $
   Language:  C++
-  Date:      $Date: 1998-10-15 11:44:20 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 1998-11-12 14:04:08 $
+  Version:   $Revision: 1.4 $
 
 
 Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -73,6 +73,7 @@ void vtkSelectPolyData::Execute()
   int numPts, numLoopPts;
   vtkPolyData *input=this->GetInput();
   vtkPolyData *output=this->GetOutput();
+  vtkPolyData *triMesh;
   vtkPointData *inPD, *outPD=output->GetPointData();
   vtkCellData *inCD, *outCD=output->GetCellData();
   int i, j, k, closest, numPolys;
@@ -112,15 +113,15 @@ void vtkSelectPolyData::Execute()
   tf->PassLinesOff();
   tf->PassVertsOff();
   tf->Update();
-  this->Mesh = tf->GetOutput();
-  this->Mesh->Register(this);
+  triMesh = tf->GetOutput();
+  triMesh->Register(this);
   tf->Delete();
-  inPD = this->Mesh->GetPointData();
-  inCD = this->Mesh->GetCellData();
+  inPD = triMesh->GetPointData();
+  inCD = triMesh->GetCellData();
 
-  numPts = this->Mesh->GetNumberOfPoints();
-  inPts = this->Mesh->GetPoints();
-  inPolys = this->Mesh->GetPolys();
+  numPts = triMesh->GetNumberOfPoints();
+  inPts = triMesh->GetPoints();
+  inPolys = triMesh->GetPolys();
   numPolys = inPolys->GetNumberOfCells();
   if ( numPolys < 1 )
     {
@@ -513,6 +514,7 @@ void vtkSelectPolyData::Execute()
     }
     
   // Clean up and update output
+  triMesh->UnRegister(this);
   this->Mesh->Delete();
   neighbors->Delete();
   edgeIds->Delete();
