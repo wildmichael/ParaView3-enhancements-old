43a0c20440d0fbb84ea1aa3e78b23e251abc678d
lorensen <lorensen>
1998-11-20 21:24:13 +0000

    ENH: better gestimate of # of output cells; progress interval adapts to number of cells

--- VTK/Graphics/vtkTriangleFilter.cxx
+++ VTK/Graphics/vtkTriangleFilter.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkTriangleFilter.cxx,v $
   Language:  C++
-  Date:      $Date: 1998-11-03 01:17:23 $
-  Version:   $Revision: 1.27 $
+  Date:      $Date: 1998-11-20 21:24:13 $
+  Version:   $Revision: 1.28 $
 
 
 Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -53,12 +53,20 @@ void vtkTriangleFilter::Execute()
   vtkCellData *inCD=input->GetCellData();
   vtkCellData *outCD=output->GetCellData();
   vtkCell *cell;
+  int updateInterval;
+  int numPoints=input->GetNumberOfPoints();
 
-  output->Allocate(3*numCells, numCells);
+  
+  output->Allocate(numPoints, numPoints);
 
+  updateInterval = numCells/100.0;
+  if (updateInterval < 1)
+    {
+    updateInterval = 1;
+    }
   for (cellNum=0; cellNum < numCells; cellNum++)
     {
-    if ( ! (cellNum % 5000) ) //manage progress reports / early abort
+    if ( ! (cellNum % updateInterval) ) //manage progress reports / early abort
       {
       this->UpdateProgress ((float)cellNum / numCells);
       if ( this->GetAbortExecute() ) 
