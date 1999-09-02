ef1f570a55715f2d2e1cc604baae6c35721bac85
lawcc <lawcc>
1999-09-02 13:01:25 +0000

    Finishing information for streaming.

--- VTK/Imaging/vtkImageSinusoidSource.cxx
+++ VTK/Imaging/vtkImageSinusoidSource.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageSinusoidSource.cxx,v $
   Language:  C++
-  Date:      $Date: 1999-07-22 12:13:53 $
-  Version:   $Revision: 1.14 $
+  Date:      $Date: 1999-09-02 13:01:26 $
+  Version:   $Revision: 1.15 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder,ill Lorensen.
@@ -140,9 +140,27 @@ void vtkImageSinusoidSource::SetWholeExtent(int xMin, int xMax,
 //----------------------------------------------------------------------------
 void vtkImageSinusoidSource::UpdateInformation()
 {
-  this->GetOutput()->SetWholeExtent(this->WholeExtent);
-  this->GetOutput()->SetScalarType(VTK_FLOAT);
-  this->GetOutput()->SetNumberOfScalarComponents(1);
+  vtkImageData *output = this->GetOutput();
+  unsigned long mem;
+
+  output->SetWholeExtent(this->WholeExtent);
+  output->SetScalarType(VTK_FLOAT);
+  output->SetNumberOfScalarComponents(1);
+
+  // What if we are trying to process a VERY large 2D image?
+  mem = output->GetScalarSize();
+  mem = mem * (this->WholeExtent[1] - this->WholeExtent[0] + 1);
+  mem = mem * (this->WholeExtent[3] - this->WholeExtent[2] + 1);
+  mem = mem / 1000;
+  mem = mem * (this->WholeExtent[5] - this->WholeExtent[4] + 1);
+  if (mem < 1)
+    {
+    mem = 1;
+    }
+  
+  output->SetEstimatedWholeMemorySize(mem);
+  // Do not allow less than 1Kb per piece.
+  output->SetMaximumNumberOfPieces(mem);
 }
 
 void vtkImageSinusoidSource::Execute(vtkImageData *data)
