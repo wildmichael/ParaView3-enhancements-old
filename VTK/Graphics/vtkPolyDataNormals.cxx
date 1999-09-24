7895a44e864675ba999c2a7b953bee6f5a85ec67
lorensen <lorensen>
1999-09-24 18:46:04 +0000

    ERR: Was not passing cell data.

--- VTK/Graphics/vtkPolyDataNormals.cxx
+++ VTK/Graphics/vtkPolyDataNormals.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkPolyDataNormals.cxx,v $
   Language:  C++
-  Date:      $Date: 1999-09-04 13:33:20 $
-  Version:   $Revision: 1.18 $
+  Date:      $Date: 1999-09-24 18:46:04 $
+  Version:   $Revision: 1.19 $
 
 
 Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -81,6 +81,7 @@ void vtkPolyDataNormals::Execute()
   vtkPoints *newPts = NULL;
   vtkNormals *newNormals;
   vtkPointData *pd, *outPD;
+  vtkCellData *cd, *outCD;
   float n[3];
   vtkCellArray *newPolys;
   int ptId, oldId;
@@ -149,6 +150,9 @@ void vtkPolyDataNormals::Execute()
   
   pd = input->GetPointData();
   outPD = output->GetPointData();
+    
+  cd = input->GetCellData();
+  outCD = output->GetCellData();
     
   NewMesh = vtkPolyData::New();
   NewMesh->SetPoints(inPts);
@@ -364,6 +368,10 @@ void vtkPolyDataNormals::Execute()
     output->SetPoints(newPts);
     newPts->Delete();
     }
+
+  // Since celss are preserved (although the order of the ids could change)
+  // we can just pass the cell data from the input
+  outCD->PassData(cd);
 
   outPD->SetNormals(newNormals);
   newNormals->Delete();
