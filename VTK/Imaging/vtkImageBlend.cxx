35ebaccb3f2d0171dd5f758468a8bf1991868f80
lorensen <lorensen>
1999-11-23 19:38:41 +0000

    ERR: SetOpacity was not modified time.

--- VTK/Imaging/vtkImageBlend.cxx
+++ VTK/Imaging/vtkImageBlend.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageBlend.cxx,v $
   Language:  C++
-  Date:      $Date: 1999-11-19 00:15:50 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 1999-11-23 19:38:41 $
+  Version:   $Revision: 1.2 $
   Thanks:    Thanks to David G. Gobbi who developed this class.
 
 Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -107,8 +107,13 @@ void vtkImageBlend::SetOpacity(int idx, double opacity)
       }
     delete [] this->Opacity;
     this->Opacity = tmp;
+    this->Opacity[idx] = 0.0;
     }
-  this->Opacity[idx] = opacity;
+  if (this->Opacity[idx] != opacity)
+    {
+    this->Opacity[idx] = opacity;
+    this->Modified();
+    }
 }
     
 //----------------------------------------------------------------------------
@@ -262,7 +267,7 @@ static void vtkImageBlendExecute(vtkImageBlend *self, int id,
   inData->GetContinuousIncrements(inExt, inIncX, inIncY, inIncZ);
   outData->GetContinuousIncrements(inExt, outIncX, outIncY, outIncZ);
 
-  // Loop through ouput pixels
+  // Loop through output pixels
   for (idxZ = 0; idxZ < maxZ; idxZ++)
     {
     for (idxY = 0; !self->AbortExecute && idxY < maxY; idxY++)
