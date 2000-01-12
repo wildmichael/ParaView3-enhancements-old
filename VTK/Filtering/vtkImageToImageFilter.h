88a9aa365918af23df38221b651d34ea05f4e718
avila <avila>
2000-01-12 15:00:17 +0000

    ERR: Removed warnings.

--- VTK/Filtering/vtkImageToImageFilter.h
+++ VTK/Filtering/vtkImageToImageFilter.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageToImageFilter.h,v $
   Language:  C++
-  Date:      $Date: 2000-01-07 09:11:35 $
-  Version:   $Revision: 1.12 $
+  Date:      $Date: 2000-01-12 15:00:17 $
+  Version:   $Revision: 1.13 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
@@ -140,10 +140,6 @@ protected:
 
   // This is the method you should override.
   virtual void Execute(vtkImageData *inData, vtkImageData *outData);
-
-  virtual void ComputeRequiredInputUpdateExtent(int inExt[6],int outExt[6])
-    {vtkErrorMacro( << "Obsolete: Use ComputeInputUpdateExtents instead" ); };
-
 
   void ComputeInputUpdateExtents( vtkDataObject *output );
   virtual void ComputeInputUpdateExtent(int inExt[6], int outExt[6]);
