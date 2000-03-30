389d9e7aadcca561263f8949164c60a7eaf0392b
millerjv <millerjv>
2000-03-30 01:08:27 +0000

    Automated commit to force versioning.

--- VTK/Common/vtkVersion.h
+++ VTK/Common/vtkVersion.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkVersion.h,v $
   Language:  C++
-  Date:      $Date: 2000-03-29 01:08:14 $
-  Version:   $Revision: 1.470 $
+  Date:      $Date: 2000-03-30 01:08:27 $
+  Version:   $Revision: 1.471 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -59,7 +59,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VTK_MAJOR_VERSION 3
 #define VTK_MINOR_VERSION 1
 #define VTK_BUILD_VERSION 1
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.470 $, $Date: 2000-03-29 01:08:14 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.471 $, $Date: 2000-03-30 01:08:27 $ (GMT)"
 
 
 class VTK_EXPORT vtkVersion : public vtkObject {
