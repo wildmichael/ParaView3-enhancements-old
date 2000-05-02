5a8c42ef39a1100acd9cd4a16ce6ed262b43a633
dgobbi <dgobbi>
2000-05-02 17:24:19 +0000

    ERR: solarisCoverage compiler is confused by const/non const template args

--- VTK/Common/vtkMath.cxx
+++ VTK/Common/vtkMath.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkMath.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-05-02 15:08:22 $
-  Version:   $Revision: 1.60 $
+  Date:      $Date: 2000-05-02 17:24:19 $
+  Version:   $Revision: 1.61 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -1415,8 +1415,8 @@ void vtkMath::LUSolve3x3(const double A[3][3],
 
 //----------------------------------------------------------------------------
 // this method solves Ay = x for y
-template<class T, class T2, class T3>
-static inline void vtkLinearSolve3x3(T A[3][3], T2 x[3], T3 y[3])
+template<class T1, class T2, class T3>
+static inline void vtkLinearSolve3x3(const T1 A[3][3], const T2 x[3], T3 y[3])
 {
   int index[3];
   T3 B[3][3];
@@ -1445,8 +1445,8 @@ void vtkMath::LinearSolve3x3(const double A[3][3],
 }
 
 //----------------------------------------------------------------------------
-template<class T, class T2, class T3>
-static inline void vtkMultiply3x3(T A[3][3], T2 v[3], T3 u[3])
+template<class T1, class T2, class T3>
+static inline void vtkMultiply3x3(const T1 A[3][3], const T2 v[3], T3 u[3])
 {
   T3 x = A[0][0]*v[0] + A[0][1]*v[1] + A[0][2]*v[2];
   T3 y = A[1][0]*v[0] + A[1][1]*v[1] + A[1][2]*v[2];
@@ -1469,7 +1469,8 @@ void vtkMath::Multiply3x3(const double A[3][3], const double v[3], double u[3])
 
 //----------------------------------------------------------------------------
 template<class T, class T2, class T3>
-static inline void vtkMultiplyMatrix3x3(T A[3][3], T2 B[3][3], T3 C[3][3])
+static inline void vtkMultiplyMatrix3x3(const T A[3][3], const T2 B[3][3],
+                                        T3 C[3][3])
 {
   T3 D[3][3];
 
