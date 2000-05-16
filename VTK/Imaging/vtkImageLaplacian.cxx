3dd35a89cf98bd5f9c11feff8fbfb251a1b93da9
martink <martink>
2000-05-16 19:54:27 +0000

    uses new vtkTemplateMacro

--- VTK/Imaging/vtkImageLaplacian.cxx
+++ VTK/Imaging/vtkImageLaplacian.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageLaplacian.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-04-28 18:13:44 $
-  Version:   $Revision: 1.19 $
+  Date:      $Date: 2000-05-16 19:54:27 $
+  Version:   $Revision: 1.20 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -243,56 +243,9 @@ void vtkImageLaplacian::ThreadedExecute(vtkImageData *inData,
   
   switch (inData->GetScalarType())
     {
-    case VTK_DOUBLE:
-      vtkImageLaplacianExecute(this, 
-			       inData, (double *)(inPtr), 
-			       outData, (double *)(outPtr), outExt, id);
-      break;
-    case VTK_FLOAT:
-      vtkImageLaplacianExecute(this, 
-			       inData, (float *)(inPtr), 
-			       outData, (float *)(outPtr), outExt, id);
-      break;
-    case VTK_LONG:
-      vtkImageLaplacianExecute(this, 
-			       inData, (long *)(inPtr), 
-			       outData, (long *)(outPtr), outExt, id);
-      break;
-    case VTK_UNSIGNED_LONG:
-      vtkImageLaplacianExecute(this, 
-			       inData, (unsigned long *)(inPtr), 
-			       outData, (unsigned long *)(outPtr), outExt, id);
-      break;
-    case VTK_INT:
-      vtkImageLaplacianExecute(this, 
-			       inData, (int *)(inPtr), 
-			       outData, (int *)(outPtr), outExt, id);
-      break;
-    case VTK_UNSIGNED_INT:
-      vtkImageLaplacianExecute(this, 
-			       inData, (unsigned int *)(inPtr), 
-			       outData, (unsigned int *)(outPtr), outExt, id);
-      break;
-    case VTK_SHORT:
-      vtkImageLaplacianExecute(this, 
-			       inData, (short *)(inPtr), 
-			       outData, (short *)(outPtr), outExt, id);
-      break;
-    case VTK_UNSIGNED_SHORT:
-      vtkImageLaplacianExecute(this, 
-			       inData, (unsigned short *)(inPtr), 
-			       outData, (unsigned short *)(outPtr), outExt, id);
-      break;
-    case VTK_CHAR:
-      vtkImageLaplacianExecute(this, 
-			       inData, (char *)(inPtr), 
-			       outData, (char *)(outPtr), outExt, id);
-      break;
-    case VTK_UNSIGNED_CHAR:
-      vtkImageLaplacianExecute(this, 
-			       inData, (unsigned char *)(inPtr), 
-			       outData, (unsigned char *)(outPtr), outExt, id);
-      break;
+    vtkTemplateMacro7(vtkImageLaplacianExecute, this, inData, 
+                      (VTK_TT *)(inPtr), outData, (VTK_TT *)(outPtr), 
+                      outExt, id);
     default:
       vtkErrorMacro(<< "Execute: Unknown ScalarType");
       return;
