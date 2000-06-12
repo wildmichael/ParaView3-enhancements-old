3001bdc2475bd279b12048f1346be882e98a714a
dgobbi <dgobbi>
2000-06-12 16:22:24 +0000

    ERR: A really ugly and simple backwards compatibility fix, a better fix      than this one will have to be found sometime soon.
         Now the GetProp3Ds() and GetActors() method return the same
         list (i.e. if you call GetActors() but have picked some
         non-actor Prop3Ds then sparks might fly)

--- VTK/Rendering/vtkPicker.cxx
+++ VTK/Rendering/vtkPicker.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkPicker.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-06-11 11:54:34 $
-  Version:   $Revision: 1.56 $
+  Date:      $Date: 2000-06-12 16:22:24 $
+  Version:   $Revision: 1.57 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -362,6 +362,10 @@ int vtkPicker::Pick(float selectionX, float selectionY, float selectionZ,
               ((1.0 - t)*p1World[0] + t*p2World[0],
                (1.0 - t)*p1World[1] + t*p2World[1],
                (1.0 - t)*p1World[2] + t*p2World[2]);
+	    // this next line is for backwards compatibility only,
+	    // what it does is an extreme no-no and a better backwards
+	    // compatibility fix will have to be found
+	    this->Actors->AddItem((vtkActor *)prop);
             }
           }
 
