d7dfd45b98319044b5f3eb545f877df2d2e34424
avila <avila>
2000-07-07 10:37:53 +0000

    fixed problems with zero length strings and increased cache

--- VTK/Rendering/vtkXTextMapper.cxx
+++ VTK/Rendering/vtkXTextMapper.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkXTextMapper.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-06-30 13:38:49 $
-  Version:   $Revision: 1.23 $
+  Date:      $Date: 2000-07-07 10:37:53 $
+  Version:   $Revision: 1.24 $
   Thanks:    Thanks to Matt Turek who developed this class.
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -132,7 +132,7 @@ void vtkXTextMapper::GetSize(vtkViewport* viewport, int *s)
     {
     this->ViewportSize[0] = vSize[0];
     this->ViewportSize[1] = vSize[1];    
-    DetermineSize(viewport, s);
+    this->DetermineSize(viewport, s);
     this->SizeMTime.Modified();
     this->Size[0] = s[0];
     this->Size[1] = s[1];
@@ -152,7 +152,7 @@ void vtkXTextMapper::DetermineSize(vtkViewport *viewport, int *size)
     return;
     }
 
-  if (this->Input == NULL)
+  if (this->Input == NULL || this->Input[0] == '\0')
     {
     size[0] = 0;
     size[1] = 0;
