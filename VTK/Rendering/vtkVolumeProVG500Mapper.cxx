a195034651f20cec098b41f719e5201715265a4d
avila <avila>
2000-07-13 16:27:03 +0000

    Some improvements to gradient opacity

--- VTK/Rendering/vtkVolumeProVG500Mapper.cxx
+++ VTK/Rendering/vtkVolumeProVG500Mapper.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkVolumeProVG500Mapper.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-06-07 15:55:22 $
-  Version:   $Revision: 1.9 $
+  Date:      $Date: 2000-07-13 16:27:03 $
+  Version:   $Revision: 1.10 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -395,7 +395,10 @@ void vtkVolumeProVG500Mapper::UpdateProperties( vtkRenderer *vtkNotUsed(ren),
 
   // Set up the gradient magnitude opacity modulation
   goFunc = vol->GetProperty()->GetGradientOpacity();
-  if ( !this->GradientOpacityModulation || !goFunc )
+
+  if ( !this->GradientOpacityModulation || !goFunc ||
+       ( !strcmp(goFunc->GetType(), "Constant") && 
+	 goFunc->GetValue(0) == 1.0 ))
     {
     this->Context->SetGradientOpacityModulation( VLIfalse );
     }
@@ -404,20 +407,20 @@ void vtkVolumeProVG500Mapper::UpdateProperties( vtkRenderer *vtkNotUsed(ren),
     switch ( this->VolumeDataType )
       {
       case VTK_VOLUME_8BIT:
-        scale = sqrt(3.0)*255.0;
+        scale = sqrt(3.0)*256.0;
         break;
       case VTK_VOLUME_12BIT_LOWER:
-        scale = sqrt(3.0)*4095;
+        scale = sqrt(3.0)*4096;
         break;
       case VTK_VOLUME_12BIT_UPPER:
-        scale = sqrt(3.0)*65535;
+        scale = sqrt(3.0)*65536;
       }
 
     gradientTable = new double [this->GradientTableSize];
     for ( i = 0; i < this->GradientTableSize; i++ )
       {
-      gradientTable[i] =
-	goFunc->GetValue( ((float)(i)/(this->GradientTableSize-1)) * scale );
+      gradientTable[i] = goFunc->GetValue(scale*((float)i+0.5)/
+					  (float)this->GradientTableSize);
       }
     
     this->Context->SetGradientOpacityModulation( VLItrue );
