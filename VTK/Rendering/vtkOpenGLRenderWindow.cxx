5462cf182418d55b53cb304d5000774db5b49069
lawcc <lawcc>
2000-08-18 19:05:13 +0000

    This will fix the basic problem,  CurrentRenderWindow needs to be replace with a local check in vtkWin32OpenGlRenderWindow

--- VTK/Rendering/vtkOpenGLRenderWindow.cxx
+++ VTK/Rendering/vtkOpenGLRenderWindow.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkOpenGLRenderWindow.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-08-18 17:17:34 $
-  Version:   $Revision: 1.44 $
+  Date:      $Date: 2000-08-18 19:05:13 $
+  Version:   $Revision: 1.45 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -1176,13 +1176,6 @@ void vtkOpenGLRenderWindow::SetZbufferData( int x1, int y1, int x2, int y2,
 
 void vtkOpenGLRenderWindow::MakeCurrent()
 {
-  // Try to avoid this call when it is not necessary.
-  if (this->CurrentRenderWindow == this)
-    {
-    return;
-    }
-  this->CurrentRenderWindow = this;
-
   // when debugging XErrors uncomment the following lines
   //if (this->DisplayId)
   //  {
