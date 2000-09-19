a6b3b78fa9c43ca540fc80a618f1f17c58b776e7
martink <martink>
2000-09-19 15:29:33 +0000

    ENH: add support for vtkDebugLeaks

--- VTK/Common/vtkObjectFactoryCollection.h
+++ VTK/Common/vtkObjectFactoryCollection.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkObjectFactoryCollection.h,v $
   Language:  C++
-  Date:      $Date: 2000-09-01 07:23:19 $
-  Version:   $Revision: 1.8 $
+  Date:      $Date: 2000-09-19 15:29:33 $
+  Version:   $Revision: 1.9 $
   Thanks:    Thanks to William A. Hoffman who developed this class
 
 
@@ -53,12 +53,20 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "vtkCollection.h"
 #include "vtkObjectFactory.h"
+#ifdef VTK_DEBUG_LEAKS
+#include "vtkDebugLeaks.h"
+#endif
 
 class VTK_EXPORT vtkObjectFactoryCollection : public vtkCollection
 {
 public:
   vtkTypeMacro(vtkObjectFactoryCollection,vtkCollection);
-  static vtkObjectFactoryCollection *New() {return new vtkObjectFactoryCollection;};
+  static vtkObjectFactoryCollection *New() 
+    {
+#ifdef VTK_DEBUG_LEAKS
+    vtkDebugLeaks::ConstructClass("vtkObjectFactoryCollection");
+#endif    
+return new vtkObjectFactoryCollection;};
 
   // Description:
   // Add an ObjectFactory from the list.
