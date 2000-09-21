119e24f226d26a1e32e98b653d6e1296ef087973
will <will>
2000-09-21 15:48:08 +0000

    ENH:Added method to remove all inputs

--- VTK/Common/vtkProcessObject.cxx
+++ VTK/Common/vtkProcessObject.cxx
@@ -3,8 +3,8 @@
  Program:   Visualization Toolkit
  Module:    $RCSfile: vtkProcessObject.cxx,v $
  Language:  C++
- Date:      $Date: 2000-05-15 20:16:40 $
- Version:   $Revision: 1.14 $
+ Date:      $Date: 2000-09-21 15:48:08 $
+ Version:   $Revision: 1.15 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -42,9 +42,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkProcessObject.h"
 #include "vtkObjectFactory.h"
 
-
-
-//------------------------------------------------------------------------------
+//-------------------------------------------------------------------------
 vtkProcessObject* vtkProcessObject::New()
 {
   // First try to create the object from the vtkObjectFactory
@@ -56,9 +54,6 @@ vtkProcessObject* vtkProcessObject::New()
   // If the factory was unable to create the object, then create it here.
   return new vtkProcessObject;
 }
-
-
-
 
 // Instantiate object with no start, end, or progress methods.
 vtkProcessObject::vtkProcessObject()
@@ -395,6 +390,26 @@ void vtkProcessObject::SetEndMethodArgDelete(void (*f)(void *))
     }
 }
 
+void vtkProcessObject::RemoveAllInputs()
+{
+  if ( this->Inputs )
+    {
+    for (int idx = 0; idx < this->NumberOfInputs; ++idx)
+      {
+      if ( this->Inputs[idx] )
+        {
+        this->Inputs[idx]->UnRegister(this);
+        this->Inputs[idx] = NULL;
+        }
+      }
+
+    delete [] this->Inputs;
+    this->Inputs = NULL;
+    this->NumberOfInputs = 0;
+    this->Modified();
+    }
+}
+
 void vtkProcessObject::PrintSelf(ostream& os, vtkIndent indent)
 {
   vtkObject::PrintSelf(os,indent);
@@ -445,6 +460,3 @@ void vtkProcessObject::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "AbortExecute: " << (this->AbortExecute ? "On\n" : "Off\n");
   os << indent << "Progress: " << this->Progress << "\n";
 }
-
-
-
