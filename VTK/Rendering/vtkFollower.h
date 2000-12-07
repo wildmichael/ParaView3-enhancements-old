536216cb607e97616e28b4ccc345b28f6d453163
berk <berk>
2000-12-07 15:02:26 +0000

    Legacy code removal, fixes.

--- VTK/Rendering/vtkFollower.h
+++ VTK/Rendering/vtkFollower.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkFollower.h,v $
   Language:  C++
-  Date:      $Date: 2000-12-07 14:52:09 $
-  Version:   $Revision: 1.40 $
+  Date:      $Date: 2000-12-07 15:02:26 $
+  Version:   $Revision: 1.41 $
 
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -82,6 +82,8 @@ class VTK_EXPORT vtkFollower : public vtkActor
   virtual void GetMatrix(vtkMatrix4x4 *m);
   virtual void GetMatrix(double m[16])
     {this->GetMatrix(this->Matrix); vtkMatrix4x4::DeepCopy(m,this->Matrix);};
+  virtual vtkMatrix4x4* GetMatrix()
+    {return this->vtkActor::GetMatrix();}
 
   // Description:
   // Set/Get the camera to follow. If this is not set, then the follower
@@ -98,12 +100,6 @@ class VTK_EXPORT vtkFollower : public vtkActor
   // For legacy compatibility. Do not use.
   void GetMatrix(vtkMatrix4x4 &m) 
     {VTK_LEGACY_METHOD(GetMatrix,"3.2"); this->GetMatrix(&m);}
-
-//BTX
-  virtual vtkMatrix4x4& GetMatrix() 
-    {VTK_LEGACY_METHOD(GetMatrix,"3.2"); return *(this->GetMatrixPointer());}
-//ETX
-
 #endif
   
 protected:
