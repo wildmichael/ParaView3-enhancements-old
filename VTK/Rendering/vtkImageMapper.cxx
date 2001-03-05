78c45e7e873b4dab12294672302d5a25c13ae139
lorensen <lorensen>
2001-03-05 23:36:32 +0000

    ERR: backed out last changes.

--- VTK/Rendering/vtkImageMapper.cxx
+++ VTK/Rendering/vtkImageMapper.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageMapper.cxx,v $
   Language:  C++
-  Date:      $Date: 2001-03-05 01:53:05 $
-  Version:   $Revision: 1.38 $
+  Date:      $Date: 2001-03-05 23:36:32 $
+  Version:   $Revision: 1.39 $
   Thanks:    Thanks to Matt Turek who developed this class.
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -74,17 +74,6 @@ vtkImageMapper::vtkImageMapper()
 
 vtkImageMapper::~vtkImageMapper()
 {
-  if (this->GetInput())
-    {
-    this->GetInput()->UnRegister(this);
-    this->SetInput(NULL);
-    }
-}
-
-unsigned long int vtkImageMapper::GetMTime()
-{
-  unsigned long mTime=this->vtkMapper2D::GetMTime();
-  return mTime;
 }
 
 //----------------------------------------------------------------------------
@@ -100,7 +89,14 @@ vtkImageData *vtkImageMapper::GetInput()
     {
     return NULL;
     }
+  
   return (vtkImageData *)(this->Inputs[0]);
+}
+
+unsigned long int vtkImageMapper::GetMTime()
+{
+  unsigned long mTime=this->vtkMapper2D::GetMTime();
+  return mTime;
 }
 
 void vtkImageMapper::PrintSelf(ostream& os, vtkIndent indent)
@@ -152,7 +148,7 @@ void vtkImageMapper::RenderStart(vtkViewport* viewport, vtkActor2D* actor)
   if (!actor)
     {
     vtkErrorMacro (<<"vtkImageMapper::Render - Null actor argument");
-    return;
+    return;    
     }
 
 
@@ -262,7 +258,7 @@ void vtkImageMapper::RenderStart(vtkViewport* viewport, vtkActor2D* actor)
 int vtkImageMapper::GetWholeZMin()
 {
   int *extent;
-
+  
   if ( ! this->GetInput())
     {
     return 0;
@@ -276,7 +272,7 @@ int vtkImageMapper::GetWholeZMin()
 int vtkImageMapper::GetWholeZMax()
 {
   int *extent;
-
+  
   if ( ! this->GetInput())
     {
     return 0;
