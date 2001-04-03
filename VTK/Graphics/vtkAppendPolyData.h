9241ae1258ac89ba84ef22a72e752f910deab171
biddi <biddi>
2001-04-03 18:11:44 +0000

    ENH: Added ability to append datasets which have a mixture of float and double point types. Only works on Points - not scalars vectors, tensors etc etc. Might try a templatized version for the rest at a later date.

--- VTK/Graphics/vtkAppendPolyData.h
+++ VTK/Graphics/vtkAppendPolyData.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkAppendPolyData.h,v $
   Language:  C++
-  Date:      $Date: 2000-12-10 20:08:30 $
-  Version:   $Revision: 1.43 $
+  Date:      $Date: 2001-04-03 18:11:44 $
+  Version:   $Revision: 1.44 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -126,6 +126,7 @@ protected:
 
   // An efficient way to append data/cells.
   void AppendData(vtkDataArray *dest, vtkDataArray *src, int offset);
+  void AppendDifferentPoints(vtkDataArray *dest, vtkDataArray *src, int offset);
   int *AppendCells(int *pDest, vtkCellArray *src, int offset);
 
  private:
