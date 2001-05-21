c46c13c4030575e133c22e1855bbd080287520c3
will <will>
2001-05-21 15:49:39 +0000

    ENH:Use new vtkOrderedTriangulator. Simpler code; 2.5x faster.

--- VTK/Graphics/vtkClipVolume.h
+++ VTK/Graphics/vtkClipVolume.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkClipVolume.h,v $
   Language:  C++
-  Date:      $Date: 2000-12-10 20:08:32 $
-  Version:   $Revision: 1.21 $
+  Date:      $Date: 2001-05-21 15:49:39 $
+  Version:   $Revision: 1.22 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -81,7 +81,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkImplicitFunction.h"
 
 class vtkMergePoints;
-class vtkDelaunay3D;
+class vtkOrderedTriangulator;
 
 class VTK_EXPORT vtkClipVolume : public vtkStructuredPointsToUnstructuredGridFilter
 {
@@ -161,13 +161,6 @@ public:
   // Return the mtime also considering the locator and clip function.
   unsigned long int GetMTime();
 
-#ifndef VTK_REMOVE_LEGACY_CODE
-  // Description:
-  // For legacy compatibility. Do not use.
-  void SetLocator(vtkPointLocator& locator) 
-    {VTK_LEGACY_METHOD(SetLocator,"3.2"); this->SetLocator(&locator);}
-#endif
-  
 protected:
   vtkClipVolume(vtkImplicitFunction *cf=NULL);
   ~vtkClipVolume();
@@ -191,9 +184,8 @@ protected:
   vtkUnstructuredGrid *ClippedOutput;
   
 private:
-  vtkUnstructuredGrid *Mesh;
-  vtkMergePoints *MeshLocator;
-  vtkDelaunay3D *Triangulator;
+  vtkUnstructuredGrid    *Mesh;
+  vtkOrderedTriangulator *Triangulator;
   
 };
 
