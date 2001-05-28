ae3471291ea734d6d40381c6dbcda56911161a3b
will <will>
2001-05-28 05:32:46 +0000

    ENH:Removed legacy code

--- VTK/Common/vtkVoidArray.h
+++ VTK/Common/vtkVoidArray.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkVoidArray.h,v $
   Language:  C++
-  Date:      $Date: 2001-04-18 11:11:48 $
-  Version:   $Revision: 1.41 $
+  Date:      $Date: 2001-05-28 05:32:46 $
+  Version:   $Revision: 1.42 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -150,13 +150,6 @@ public:
   // Description:
   // Deep copy of another void array.
   void DeepCopy(vtkDataArray *da);
-  
-#ifndef VTK_REMOVE_LEGACY_CODE
-  // Description:
-  // For legacy compatibility. Do not use.
-  void DeepCopy(vtkDataArray &da) 
-    {VTK_LEGACY_METHOD(DeepCopy,"3.2"); this->DeepCopy(&da);}
-#endif
   
 
 protected:
