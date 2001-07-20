d897711ddf35b802c59adc86ca9d06252bc08828
lorensen <lorensen>
2001-07-20 03:55:13 +0000

    ENH: rearrange cout's to fool PrintSelf defect checker.

--- VTK/Imaging/vtkImageAccumulate.cxx
+++ VTK/Imaging/vtkImageAccumulate.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageAccumulate.cxx,v $
   Language:  C++
-  Date:      $Date: 2001-07-18 12:40:03 $
-  Version:   $Revision: 1.33 $
+  Date:      $Date: 2001-07-20 03:55:13 $
+  Version:   $Revision: 1.34 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -442,6 +442,15 @@ void vtkImageAccumulate::PrintSelf(ostream& os, vtkIndent indent)
 {
   vtkImageToImageFilter::PrintSelf(os,indent);
 
+  os << indent << "Mean: " << this->Mean << "\n";
+  os << indent << "Min: " << this->Min << "\n";
+  os << indent << "Max: " << this->Max << "\n";
+  os << indent << "PixelCount: " << this->PixelCount << "\n";
+  os << indent << "StencilFunction: " << this->StencilFunction << "\n";
+  os << indent << "ReverseStencil: " << (this->ReverseStencil ?
+		                         "On\n" : "Off\n");
+  os << indent << "ClippingExtents: " << this->ClippingExtents << "\n";
+
   os << indent << "ComponentOrigin: ( "
      << this->ComponentOrigin[0] << ", "
      << this->ComponentOrigin[1] << ", "
@@ -456,16 +465,5 @@ void vtkImageAccumulate::PrintSelf(ostream& os, vtkIndent indent)
      << this->ComponentExtent[0] << "," << this->ComponentExtent[1] << " "
      << this->ComponentExtent[2] << "," << this->ComponentExtent[3] << " "
      << this->ComponentExtent[4] << "," << this->ComponentExtent[5] << " }\n";
-
-  os << indent << "StencilFunction: " << this->StencilFunction << "\n";
-  os << indent << "ReverseStencil: " << (this->ReverseStencil ?
-		                         "On\n" : "Off\n");
-
-  os << indent << "ClippingExtents: " << this->ClippingExtents << "\n";
-  
-  os << indent << "Mean: " << this->Mean << "\n";
-  os << indent << "Min: " << this->Min << "\n";
-  os << indent << "Max: " << this->Max << "\n";
-  os << indent << "PixelCount: " << this->PixelCount << "\n";
 }
 
