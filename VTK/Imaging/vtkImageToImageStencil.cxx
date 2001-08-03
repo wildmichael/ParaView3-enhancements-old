4ed29a02dd44fce3d192d32efdd49d0ed76e9281
berk <berk>
2001-08-03 13:10:38 +0000

    Should not include something in graphics.

--- VTK/Imaging/vtkImageToImageStencil.cxx
+++ VTK/Imaging/vtkImageToImageStencil.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageToImageStencil.cxx,v $
   Language:  C++
-  Date:      $Date: 2001-08-02 18:48:42 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2001-08-03 13:10:38 $
+  Version:   $Revision: 1.2 $
   Thanks:    Thanks to David G Gobbi who developed this class.
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -43,7 +43,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <math.h>
 #include "vtkImageToImageStencil.h"
 #include "vtkPolyData.h"
-#include "vtkOBBTree.h"
 #include "vtkObjectFactory.h"
 
 
