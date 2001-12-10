db6f32a92950d2986ac7d1089e5b4e5a46d63309
lawcc <lawcc>
2001-12-10 21:27:53 +0000

    Operating on any array.

--- VTK/Graphics/vtkThreshold.h
+++ VTK/Graphics/vtkThreshold.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkThreshold.h,v $
   Language:  C++
-  Date:      $Date: 2001-10-11 13:37:19 $
-  Version:   $Revision: 1.40 $
+  Date:      $Date: 2001-12-10 21:27:53 $
+  Version:   $Revision: 1.41 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -115,9 +115,10 @@ public:
   
   // Description:
   // If you want to threshold by an arbitrary array, then set its name here.
-  // By default this in NULL which implies use scalars.
-  vtkSetStringMacro(ArrayName);
-  vtkGetStringMacro(ArrayName);
+  // By default this in NULL and the filter will use the active scalar array.
+  vtkGetStringMacro(InputScalarsSelection);
+  void SelectInputScalars(const char *fieldName) 
+    {this->SetInputScalarsSelection(fieldName);}
   
 protected:
   vtkThreshold();
@@ -140,7 +141,9 @@ protected:
   int Between(float s) {return ( s >= this->LowerThreshold ? 
                                ( s <= this->UpperThreshold ? 1 : 0 ) : 0 );};
 
-  char *ArrayName;
+  char *InputScalarsSelection;
+  vtkSetStringMacro(InputScalarsSelection);
+
 private:
   vtkThreshold(const vtkThreshold&);  // Not implemented.
   void operator=(const vtkThreshold&);  // Not implemented.
