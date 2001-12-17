36b46db1190e33358254c1bfaa9b3ee633d9584e
blezek <blezek>
2001-12-17 19:09:27 +0000

    ENH: better documentation.

--- VTK/Patented/vtkMarchingCubes.h
+++ VTK/Patented/vtkMarchingCubes.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkMarchingCubes.h,v $
   Language:  C++
-  Date:      $Date: 2001-12-17 18:55:52 $
-  Version:   $Revision: 1.54 $
+  Date:      $Date: 2001-12-17 19:09:27 $
+  Version:   $Revision: 1.55 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -123,7 +123,7 @@ public:
 
   // Description:
   // Overide the default locator.  Useful for changing the number of
-  // bins or specifying a more aggressive locator.
+  // bins for performance or specifying a more aggressive locator.
   void SetLocator(vtkPointLocator *locator);
   vtkGetObjectMacro(Locator,vtkPointLocator);
 
