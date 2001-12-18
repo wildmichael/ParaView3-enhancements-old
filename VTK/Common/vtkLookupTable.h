c900c7d21c42f73a398110736e02cdd6a8554ee9
millerjv <millerjv>
2001-12-18 14:17:22 +0000

    Force update to check watch list

--- VTK/Common/vtkLookupTable.h
+++ VTK/Common/vtkLookupTable.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkLookupTable.h,v $
   Language:  C++
-  Date:      $Date: 2001-12-18 13:50:12 $
-  Version:   $Revision: 1.62 $
+  Date:      $Date: 2001-12-18 14:17:22 $
+  Version:   $Revision: 1.63 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
