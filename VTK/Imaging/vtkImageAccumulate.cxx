7961a248830cfdec49549d271fdc603b86026786
lorensen <lorensen>
2002-01-14 14:44:01 +0000

    ERR: was not computing sum of squares needed for variance computation.

--- VTK/Imaging/vtkImageAccumulate.cxx
+++ VTK/Imaging/vtkImageAccumulate.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageAccumulate.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-01-14 13:44:44 $
-  Version:   $Revision: 1.42 $
+  Date:      $Date: 2002-01-14 14:44:01 $
+  Version:   $Revision: 1.43 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -45,7 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <math.h>
 #include <stdlib.h>
 
-vtkCxxRevisionMacro(vtkImageAccumulate, "$Revision: 1.42 $");
+vtkCxxRevisionMacro(vtkImageAccumulate, "$Revision: 1.43 $");
 vtkStandardNewMacro(vtkImageAccumulate);
 
 //----------------------------------------------------------------------------
@@ -235,6 +235,7 @@ static void vtkImageAccumulateExecute(vtkImageAccumulate *self,
             {
             // Gather statistics
             sum[idxC]+= *tempPtr;
+            sumSqr[idxC]+= (*tempPtr * *tempPtr);
             if (*tempPtr > Max[idxC])
               {
               Max[idxC] = *tempPtr;
@@ -269,6 +270,7 @@ static void vtkImageAccumulateExecute(vtkImageAccumulate *self,
     Mean[0] = sum[0] / (double)*VoxelCount;    
     Mean[1] = sum[1] / (double)*VoxelCount;    
     Mean[2] = sum[2] / (double)*VoxelCount;    
+
     variance = sumSqr[0] / (double)*VoxelCount - ((double) *VoxelCount * Mean[0] * Mean[0] / (double) (*VoxelCount - 1));
     StandardDeviation[0] = sqrt(variance);
     variance = sumSqr[1] / (double)*VoxelCount - ((double) *VoxelCount * Mean[1] * Mean[1] / (double) (*VoxelCount - 1));
