dfdf6ad58a7567263068329769c648aab15e12ca
will <will>
2002-01-22 15:29:00 +0000

    ENH:Updated copyright: new year and streamlined

--- VTK/Graphics/Testing/Cxx/CellLocator.cxx
+++ VTK/Graphics/Testing/Cxx/CellLocator.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: CellLocator.cxx,v $
+  Language:  C++
+  Date:      $Date: 2002-01-22 15:29:57 $
+  Version:   $Revision: 1.3 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderer.h"
--- VTK/Graphics/Testing/Cxx/expCos.cxx
+++ VTK/Graphics/Testing/Cxx/expCos.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: expCos.cxx,v $
+  Language:  C++
+  Date:      $Date: 2002-01-22 15:29:58 $
+  Version:   $Revision: 1.7 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 //
 // Brute force computation of Bessel functions. Might be better to create a
 // filter (or source) object. Might also consider vtkSampleFunction.
