190133548fd592fffadf61906e904a1e50745cec
will <will>
2002-01-22 15:34:25 +0000

    ENH:Updated copyright: new year and streamlined

--- VTK/Parallel/Testing/Cxx/MPIGroups.cxx
+++ VTK/Parallel/Testing/Cxx/MPIGroups.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: MPIGroups.cxx,v $
+  Language:  C++
+  Date:      $Date: 2002-01-22 15:35:05 $
+  Version:   $Revision: 1.9 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkMPIController.h"
 #include "vtkSphereSource.h"
 #include "vtkPolyDataNormals.h"
--- VTK/Parallel/Testing/Cxx/ThreadedCommunicator.cxx
+++ VTK/Parallel/Testing/Cxx/ThreadedCommunicator.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: ThreadedCommunicator.cxx,v $
+  Language:  C++
+  Date:      $Date: 2002-01-22 15:35:07 $
+  Version:   $Revision: 1.9 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkThreadedController.h"
 #include "vtkDoubleArray.h"
 #include "vtkCharArray.h"
