eab70d563c90b797d8f74a241f02ef8b4f4b2cc4
will <will>
2002-01-22 15:31:43 +0000

    ENH:Updated copyright: new year and streamlined

--- VTK/Imaging/Testing/Cxx/ImportExport.cxx
+++ VTK/Imaging/Testing/Cxx/ImportExport.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: ImportExport.cxx,v $
+  Language:  C++
+  Date:      $Date: 2002-01-22 15:33:51 $
+  Version:   $Revision: 1.7 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkImageViewer.h"
 #include "vtkImageReader.h"
 #include "vtkImageImport.h"
