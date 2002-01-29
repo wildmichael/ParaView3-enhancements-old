e82d17530bcd5fe58edd2e8731e44abe0bc2f1c0
berk <berk>
2002-01-29 21:47:19 +0000

    Style.

--- VTK/Parallel/vtkCompositeManager.cxx
+++ VTK/Parallel/vtkCompositeManager.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkCompositeManager.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-01-22 15:34:27 $
-  Version:   $Revision: 1.14 $
+  Date:      $Date: 2002-01-29 21:47:19 $
+  Version:   $Revision: 1.15 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -27,7 +27,7 @@
 #include "vtkMesaRenderWindow.h"
 #endif
 
-vtkCxxRevisionMacro(vtkCompositeManager, "$Revision: 1.14 $");
+vtkCxxRevisionMacro(vtkCompositeManager, "$Revision: 1.15 $");
 
 // Structures to communicate render info.
 struct vtkCompositeRenderWindowInfo 
@@ -673,7 +673,7 @@ void vtkCompositeManager::ResetCameraClippingRange(vtkRenderer *ren)
 
 //----------------------------------------------------------------------------
 void vtkCompositeManager::ComputeVisiblePropBounds(vtkRenderer *ren, 
-                                                float bounds[6])
+						   float bounds[6])
 {
   float tmp[6];
   int id, num;
