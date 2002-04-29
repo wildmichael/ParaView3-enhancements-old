1676f6c7938c84fd902ee878c16d9069d601a34b
barre <barre>
2002-04-29 22:27:16 +0000

    FIX: missing vtkGetWindowLong macro (was GetWindowLongPtr)

--- VTK/Rendering/vtkWin32RenderWindowInteractor.cxx
+++ VTK/Rendering/vtkWin32RenderWindowInteractor.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkWin32RenderWindowInteractor.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-04-29 21:25:49 $
-  Version:   $Revision: 1.81 $
+  Date:      $Date: 2002-04-29 22:27:16 $
+  Version:   $Revision: 1.82 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -49,7 +49,7 @@ VTK_RENDERING_EXPORT LRESULT CALLBACK vtkHandleMessage2(HWND,UINT,WPARAM,LPARAM,
 
 
 #ifndef VTK_IMPLEMENT_MESA_CXX
-vtkCxxRevisionMacro(vtkWin32RenderWindowInteractor, "$Revision: 1.81 $");
+vtkCxxRevisionMacro(vtkWin32RenderWindowInteractor, "$Revision: 1.82 $");
 vtkStandardNewMacro(vtkWin32RenderWindowInteractor);
 #endif
 
@@ -78,7 +78,7 @@ vtkWin32RenderWindowInteractor::~vtkWin32RenderWindowInteractor()
     {
     vtkWin32OpenGLRenderWindow *ren;
     ren = static_cast<vtkWin32OpenGLRenderWindow *>(this->RenderWindow);
-    tmp = (vtkWin32OpenGLRenderWindow *)(GetWindowLongPtr(this->WindowId,4));
+    tmp = (vtkWin32OpenGLRenderWindow *)(vtkGetWindowLong(this->WindowId,4));
     // watch for odd conditions
     if ((tmp != ren) && (ren != NULL)) 
       {
