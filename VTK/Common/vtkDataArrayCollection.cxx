169cd223c6845b844bb5b3c546811be0ac5e9817
berk <berk>
2002-06-12 11:48:09 +0000

    New collections.

---
+++ VTK/Common/vtkDataArrayCollection.cxx
@@ -0,0 +1,22 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkDataArrayCollection.cxx,v $
+  Language:  C++
+  Date:      $Date: 2002-06-12 11:48:09 $
+  Version:   $Revision: 1.1 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkDataArrayCollection.h"
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro(vtkDataArrayCollection, "$Revision: 1.1 $");
+vtkStandardNewMacro(vtkDataArrayCollection);
