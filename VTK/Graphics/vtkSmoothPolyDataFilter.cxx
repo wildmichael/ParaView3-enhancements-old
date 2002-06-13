0f18f460ed31bad88f81c9cf37bf230f18abe35c
blue <blue>
2002-06-13 11:03:46 +0000

    ERR: Was allocating an array of vtkSmoothPoints but only using one.

--- VTK/Graphics/vtkSmoothPolyDataFilter.cxx
+++ VTK/Graphics/vtkSmoothPolyDataFilter.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkSmoothPolyDataFilter.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-05-08 13:34:14 $
-  Version:   $Revision: 1.33 $
+  Date:      $Date: 2002-06-13 11:03:46 $
+  Version:   $Revision: 1.34 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -23,7 +23,7 @@
 #include "vtkObjectFactory.h"
 #include "vtkFloatArray.h"
 
-vtkCxxRevisionMacro(vtkSmoothPolyDataFilter, "$Revision: 1.33 $");
+vtkCxxRevisionMacro(vtkSmoothPolyDataFilter, "$Revision: 1.34 $");
 vtkStandardNewMacro(vtkSmoothPolyDataFilter);
 
 // The following code defines a helper class for performing mesh smoothing
@@ -499,7 +499,7 @@ void vtkSmoothPolyDataFilter::Execute()
   // constrained to the surface of the mesh object).
   if ( source )
     {
-    this->SmoothPoints = new vtkSmoothPoints[numPts];
+    this->SmoothPoints = new vtkSmoothPoints;
     vtkSmoothPoint *sPtr;
     cellLocator = vtkCellLocator::New();
     w = new float[input->GetMaxCellSize()];
@@ -597,7 +597,7 @@ void vtkSmoothPolyDataFilter::Execute()
   if ( source )
     {
     cellLocator->Delete();
-    delete [] this->SmoothPoints;
+    delete this->SmoothPoints;
     delete [] w;
     }
 
