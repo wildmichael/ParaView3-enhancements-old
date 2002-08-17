4f8b4ba7227f8559f3b46dfd203058affdc709f8
jlee <jlee>
2002-08-17 19:40:54 +0000

    ENH added global reduction methods

--- VTK/Parallel/vtkMPICommunicator.h
+++ VTK/Parallel/vtkMPICommunicator.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkMPICommunicator.h,v $
   Language:  C++
-  Date:      $Date: 2002-08-12 15:20:30 $
-  Version:   $Revision: 1.24 $
+  Date:      $Date: 2002-08-17 19:40:54 $
+  Version:   $Revision: 1.25 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -195,6 +195,27 @@ public:
               int sendlength, int* recvlengths, int* offsets, int root);
   int GatherV(double* data, double* to, 
               int sendlength, int* recvlengths, int* offsets, int root);
+
+  // Description:
+  // Reduce an array to the given root process.  
+  int ReduceMax(int* data, int* to, int size, int root);
+  int ReduceMax(long* data, long* to, int size, int root);
+  int ReduceMax(float* data, float* to, int size, int root);
+  int ReduceMax(double* data, double* to, int size, int root);
+
+  int ReduceMin(int* data, int* to, int size, int root);
+  int ReduceMin(long* data, long* to, int size, int root);
+  int ReduceMin(float* data, float* to, int size, int root);
+  int ReduceMin(double* data, double* to, int size, int root);
+
+  int ReduceSum(int* data, int* to, int size, int root);
+  int ReduceSum(long* data, long* to, int size, int root);
+  int ReduceSum(float* data, float* to, int size, int root);
+  int ReduceSum(double* data, double* to, int size, int root);
+
+  int ReduceAnd(bool* data, bool* to, int size, int root);
+  int ReduceOr(bool* data, bool* to, int size, int root);
+
 
 //BTX
 
