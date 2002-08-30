771cee20790f1560793ed5073ac5fe29c4c08ce8
andy <andy>
2002-08-30 12:17:54 +0000

    Add missing includes

--- VTK/Rendering/vtkVolumeProVG500Mapper.cxx
+++ VTK/Rendering/vtkVolumeProVG500Mapper.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkVolumeProVG500Mapper.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-08-26 22:15:15 $
-  Version:   $Revision: 1.35 $
+  Date:      $Date: 2002-08-30 12:17:54 $
+  Version:   $Revision: 1.36 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -22,17 +22,20 @@
 #include "vtkDebugLeaks.h"
 #include "vtkGraphicsFactory.h"
 #include "vtkLight.h"
+#include "vtkLightCollection.h"
 #include "vtkObjectFactory.h"
 #include "vtkOpenGLVolumeProVG500Mapper.h"
 #include "vtkPiecewiseFunction.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderer.h"
 #include "vtkToolkits.h"
+#include "vtkTransform.h"
 #include "vtkVolume.h"
+#include "vtkVolumeProperty.h"
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkVolumeProVG500Mapper, "$Revision: 1.35 $");
+vtkCxxRevisionMacro(vtkVolumeProVG500Mapper, "$Revision: 1.36 $");
 
 //----------------------------------------------------------------------------
 // Needed when we don't use the vtkStandardNewMacro.
--- VTK/Rendering/vtkVolumeProVP1000Mapper.cxx
+++ VTK/Rendering/vtkVolumeProVP1000Mapper.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkVolumeProVP1000Mapper.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-08-26 22:15:15 $
-  Version:   $Revision: 1.19 $
+  Date:      $Date: 2002-08-30 12:17:54 $
+  Version:   $Revision: 1.20 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -22,6 +22,7 @@
 #include "vtkDebugLeaks.h"
 #include "vtkGraphicsFactory.h"
 #include "vtkLight.h"
+#include "vtkLightCollection.h"
 #include "vtkObjectFactory.h"
 #include "vtkOpenGLVolumeProVP1000Mapper.h"
 #include "vtkPiecewiseFunction.h"
@@ -33,7 +34,7 @@
 #include <stdio.h>
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkVolumeProVP1000Mapper, "$Revision: 1.19 $");
+vtkCxxRevisionMacro(vtkVolumeProVP1000Mapper, "$Revision: 1.20 $");
 
 //----------------------------------------------------------------------------
 // Needed when we don't use the vtkStandardNewMacro.
