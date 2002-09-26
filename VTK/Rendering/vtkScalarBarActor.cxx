5612d96f0f391805751bbc432c257bd30bf5785f
barre <barre>
2002-09-26 17:16:19 +0000

    FIX: object was Modified() too often

--- VTK/Rendering/vtkScalarBarActor.cxx
+++ VTK/Rendering/vtkScalarBarActor.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkScalarBarActor.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-06-30 05:45:43 $
-  Version:   $Revision: 1.43 $
+  Date:      $Date: 2002-09-26 17:16:19 $
+  Version:   $Revision: 1.44 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -26,7 +26,7 @@
 #include "vtkViewport.h"
 #include "vtkWindow.h"
 
-vtkCxxRevisionMacro(vtkScalarBarActor, "$Revision: 1.43 $");
+vtkCxxRevisionMacro(vtkScalarBarActor, "$Revision: 1.44 $");
 vtkStandardNewMacro(vtkScalarBarActor);
 
 vtkCxxSetObjectMacro(vtkScalarBarActor,LookupTable,vtkScalarsToColors);
@@ -195,6 +195,7 @@ int vtkScalarBarActor::RenderOpaqueGeometry(vtkViewport *viewport)
     }
 
   // Check to see whether we have to rebuild everything
+  int positionsHaveChanged = 0;
   if (viewport->GetMTime() > this->BuildTime || 
       (viewport->GetVTKWindow() && 
        viewport->GetVTKWindow()->GetMTime() > this->BuildTime))
@@ -214,12 +215,13 @@ int vtkScalarBarActor::RenderOpaqueGeometry(vtkViewport *viewport)
         this->LastOrigin[0] != barOrigin[0] || 
         this->LastOrigin[1] != barOrigin[1])
       {
-      this->Modified();
+      positionsHaveChanged = 1;
       }
     }
   
   // Check to see whether we have to rebuild everything
-  if (this->GetMTime() > this->BuildTime || 
+  if (positionsHaveChanged ||
+      this->GetMTime() > this->BuildTime || 
       this->LookupTable->GetMTime() > this->BuildTime ||
       this->LabelTextProperty->GetMTime() > this->BuildTime ||
       this->TitleTextProperty->GetMTime() > this->BuildTime)
