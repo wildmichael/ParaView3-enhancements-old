a6d4746ff5fe26c486e1cb30785085750dfc530d
jlee <jlee>
2002-11-22 21:43:43 +0000

    ENH apparently vtkWin32OpenGLRenderWindow is adding extra space in the render window if ParentID is not set, so we set it to avoid incorrect window sizes in java.

--- VTK/Common/vtkJavaAwt.h
+++ VTK/Common/vtkJavaAwt.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkJavaAwt.h,v $
   Language:  C++
-  Date:      $Date: 2002-11-19 18:16:29 $
-  Version:   $Revision: 1.15 $
+  Date:      $Date: 2002-11-22 21:43:43 $
+  Version:   $Revision: 1.16 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -71,6 +71,8 @@ Java_vtk_vtkPanel_RenderCreate(JNIEnv *env, jobject canvas, jobject id0)
   dsi_win = (JAWT_Win32DrawingSurfaceInfo*)dsi->platformInfo;
   temp0->SetWindowId((void *)dsi_win->hwnd);
   temp0->SetDisplayId((void *)dsi_win->hdc);
+  // also set parent id to avoid border sizes being added
+  temp0->SetParentId((void *)dsi_win->hdc);
 // use mac code
 #elif defined(VTK_USE_CARBON)
   JAWT_MacDrawingSurfaceInfo* dsi_mac;
