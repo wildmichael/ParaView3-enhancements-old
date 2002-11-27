f2079bb24281ab66c728c100ee079016621efe01
will <will>
2002-11-27 11:45:44 +0000

    ERR:Include files required

--- VTK/Examples/Modelling/Cxx/finance.cxx
+++ VTK/Examples/Modelling/Cxx/finance.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: finance.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-06-25 14:03:33 $
-  Version:   $Revision: 1.5 $
+  Date:      $Date: 2002-11-27 11:45:44 $
+  Version:   $Revision: 1.6 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -29,6 +29,7 @@
 #include "vtkRenderer.h"
 #include "vtkTubeFilter.h"
 #include "vtkUnstructuredGrid.h"
+#include "vtkImageData.h"
 
 static vtkDataSet *ReadFinancialData(const char *x, const char *y, const char *z, const char *s);
 static int ParseFile(FILE *file, const char *tag, float *data);
