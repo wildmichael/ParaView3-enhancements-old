47f8134c434c5fb106f7116ac07014e1d0a776a7
will <will>
2002-12-12 13:13:09 +0000

    ERR:Make widget work properly with multiple renderers

--- VTK/Hybrid/vtkPointWidget.cxx
+++ VTK/Hybrid/vtkPointWidget.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkPointWidget.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-09-05 11:53:07 $
-  Version:   $Revision: 1.13 $
+  Date:      $Date: 2002-12-12 13:13:09 $
+  Version:   $Revision: 1.14 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -31,7 +31,7 @@
 #include "vtkRenderWindow.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkPointWidget, "$Revision: 1.13 $");
+vtkCxxRevisionMacro(vtkPointWidget, "$Revision: 1.14 $");
 vtkStandardNewMacro(vtkPointWidget);
 
 vtkPointWidget::vtkPointWidget()
@@ -301,9 +301,15 @@ void vtkPointWidget::OnLeftButtonDown()
   int X = this->Interactor->GetEventPosition()[0];
   int Y = this->Interactor->GetEventPosition()[1];
 
-  // Okay, we can process this. Pick the cursor.
+  // Okay, make sure that the pick is in the current renderer
+  vtkRenderer *ren = this->Interactor->FindPokedRenderer(X,Y);
+  if ( ren != this->CurrentRenderer )
+    {
+    this->State = vtkPointWidget::Outside;
+    return;
+    }
+  
   vtkAssemblyPath *path;
-  this->Interactor->FindPokedRenderer(X,Y);
   this->CursorPicker->Pick(X,Y,0.0,this->CurrentRenderer);
   path = this->CursorPicker->GetPath();
   if ( path != NULL )
@@ -348,10 +354,16 @@ void vtkPointWidget::OnMiddleButtonDown()
   int X = this->Interactor->GetEventPosition()[0];
   int Y = this->Interactor->GetEventPosition()[1];
 
-  // Okay, we can process this. Pick 
-  // if no handles picked, then pick the bounding box.
+  // Okay, make sure that the pick is in the current renderer
+  vtkRenderer *ren = this->Interactor->FindPokedRenderer(X,Y);
+  if ( ren != this->CurrentRenderer )
+    {
+    this->State = vtkPointWidget::Outside;
+    return;
+    }
+  
+  // Okay, we can process this.
   vtkAssemblyPath *path;
-  this->Interactor->FindPokedRenderer(X,Y);
   this->CursorPicker->Pick(X,Y,0.0,this->CurrentRenderer);
   path = this->CursorPicker->GetPath();
   if ( path != NULL )
@@ -395,9 +407,16 @@ void vtkPointWidget::OnRightButtonDown()
   int X = this->Interactor->GetEventPosition()[0];
   int Y = this->Interactor->GetEventPosition()[1];
 
+  // Okay, make sure that the pick is in the current renderer
+  vtkRenderer *ren = this->Interactor->FindPokedRenderer(X,Y);
+  if ( ren != this->CurrentRenderer )
+    {
+    this->State = vtkPointWidget::Outside;
+    return;
+    }
+  
   // Okay, we can process this. Pick the cursor.
   vtkAssemblyPath *path;
-  this->Interactor->FindPokedRenderer(X,Y);
   this->CursorPicker->Pick(X,Y,0.0,this->CurrentRenderer);
   path = this->CursorPicker->GetPath();
   if ( path != NULL )
