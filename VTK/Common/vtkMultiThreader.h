6d7acdfa14ee2fd052d33134d8259e7c09e6b11b
lorensen <lorensen>
2002-12-28 13:16:07 +0000

    ERR: comments to justify system includes.

--- VTK/Common/vtkMultiThreader.h
+++ VTK/Common/vtkMultiThreader.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkMultiThreader.h,v $
   Language:  C++
-  Date:      $Date: 2002-12-26 21:52:49 $
-  Version:   $Revision: 1.44 $
+  Date:      $Date: 2002-12-28 13:16:07 $
+  Version:   $Revision: 1.45 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -28,13 +28,13 @@
 #include "vtkObject.h"
 
 #ifdef VTK_USE_SPROC
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/types.h> // Needed for unix implementation of sproc
+#include <unistd.h> // Needed for unix implementation of sproc
 #endif
 
 #ifdef VTK_USE_PTHREADS
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/types.h> // Needed for unix implementation of pthreads
+#include <unistd.h> // Needed for unix implementation of pthreads
 #endif
 
 // If VTK_USE_SPROC is defined, then sproc() will be used to create
