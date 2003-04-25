29bd228934eca9993a32f5ec6d9e8b456b0fac5b
clarke <clarke>
2003-04-25 17:24:48 +0000

    fix to build XDMF with NDGM properly

--- Utilities/Xdmf/vtk/vtkXdmfRenderWindowInteractor.h
+++ Utilities/Xdmf/vtk/vtkXdmfRenderWindowInteractor.h
@@ -2,9 +2,9 @@
 /*                               XDMF                              */
 /*                   eXtensible Data Model and Format              */
 /*                                                                 */
-/*  Id : $Id: vtkXdmfRenderWindowInteractor.h,v 1.1 2002-12-02 17:13:54 clarke Exp $  */
-/*  Date : $Date: 2002-12-02 17:13:54 $ */
-/*  Version : $Revision: 1.1 $ */
+/*  Id : $Id: vtkXdmfRenderWindowInteractor.h,v 1.2 2003-04-25 17:24:48 clarke Exp $  */
+/*  Date : $Date: 2003-04-25 17:24:48 $ */
+/*  Version : $Revision: 1.2 $ */
 /*                                                                 */
 /*  Author:                                                        */
 /*     Jerry A. Clarke                                             */
@@ -22,23 +22,13 @@
 /*     for more information.                                       */
 /*                                                                 */
 /*******************************************************************/
-#if !defined CYGWIN
 #include <vtkXRenderWindowInteractor.h>
 
 class VTK_EXPORT vtkXdmfRenderWindowInteractor : public vtkXRenderWindowInteractor
-#else
-#include <vtkWin32RenderWindowInteractor.h>
-
-class VTK_EXPORT vtkXdmfRenderWindowInteractor : public vtkWin32RenderWindowInteractor
-#endif
 {
 public:
   static vtkXdmfRenderWindowInteractor *New();
-#if !defined CYGWIN
   vtkTypeMacro(vtkXdmfRenderWindowInteractor,vtkXRenderWindowInteractor);
-#else
-  vtkTypeMacro(vtkXdmfRenderWindowInteractor,vtkWin32RenderWindowInteractor);
-#endif
   virtual void LoopOnce();
   virtual void Start( int Block = 0 );
 };
