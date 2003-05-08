d3b6cd13033e5fdd3870e376b99b3da43a4c08a8
henderson <henderson>
2003-05-08 15:11:54 +0000

    updating the copyright; file-specific information has been removed, so now you don't have to rebuild again after doing a cvs commit

--- Common/KWCommon/vtkKWIcon.cxx
+++ Common/KWCommon/vtkKWIcon.cxx
@@ -1,12 +1,6 @@
 /*=========================================================================
 
-  Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkKWIcon.cxx,v $
-  Language:  C++
-  Date:      $Date: 2003-05-05 17:41:25 $
-  Version:   $Revision: 1.16 $
-
-Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
+Copyright (c) 1998-2003 Kitware Inc. 469 Clifton Corporate Parkway,
 Clifton Park, NY, 12065, USA.
 All rights reserved.
 
@@ -21,7 +15,7 @@ modification, are permitted provided that the following conditions are met:
    and/or other materials provided with the distribution.
 
  * Neither the name of Kitware nor the names of any contributors may be used
-   to endorse or promote products derived from this software without specific 
+   to endorse or promote products derived from this software without specific
    prior written permission.
 
  * Modified source versions must be plainly marked as such, and must not be
@@ -52,7 +46,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWIcon );
-vtkCxxRevisionMacro(vtkKWIcon, "$Revision: 1.16 $");
+vtkCxxRevisionMacro(vtkKWIcon, "$Revision: 1.17 $");
 
 //----------------------------------------------------------------------------
 vtkKWIcon::vtkKWIcon()
@@ -522,3 +516,6 @@ void vtkKWIcon::PrintSelf(ostream& os, vtkIndent indent)
      << indent << "Height: " << this->GetHeight() << endl
      << indent << "PixelSize: " << this->GetPixelSize() << endl;
 }
+
+
+
