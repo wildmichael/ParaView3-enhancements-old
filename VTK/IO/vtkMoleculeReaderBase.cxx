a86413d29464d6e3fedc4e15eb78384750c69b54
andy <andy>
2003-05-14 14:34:38 +0000

    ERR: Remove warning on Visual studio

--- VTK/IO/vtkMoleculeReaderBase.cxx
+++ VTK/IO/vtkMoleculeReaderBase.cxx
@@ -3,8 +3,8 @@
 Program:   Visualization Toolkit
 Module:    $RCSfile: vtkMoleculeReaderBase.cxx,v $
 Language:  C++
-Date:      $Date: 2003-05-14 14:23:16 $
-Version:   $Revision: 1.5 $
+Date:      $Date: 2003-05-14 14:34:38 $
+Version:   $Revision: 1.6 $
 
 
 Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
@@ -52,7 +52,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <ctype.h>
 
-vtkCxxRevisionMacro(vtkMoleculeReaderBase, "$Revision: 1.5 $");
+vtkCxxRevisionMacro(vtkMoleculeReaderBase, "$Revision: 1.6 $");
 
 static float vtkMoleculeReaderBaseCovRadius[103] = {
 0.32 , 1.6 , 0.68 , 0.352 , 0.832 , 0.72 ,
@@ -365,7 +365,7 @@ int vtkMoleculeReaderBase::MakeBonds(vtkPoints *newPts,
 int vtkMoleculeReaderBase::MakeAtomType(const char *atype)
 {
   char      a, b;
-  int       anum;
+  int       anum=0;
 
   a = atype[0];
   if (islower(a)) a = toupper(a);
