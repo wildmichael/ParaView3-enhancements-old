acf65f43650d01d1272f1a0701224290f86d02f4
barre <barre>
2003-06-06 20:39:19 +0000

    ENH: static Tcl/Tk support - provide TCL_TK_STATIC, - encapsulate #include tcl.h / tk.h inside vtkTcl.h / vtkTk.h so that we can support static Tcl/Tk - add missing imm32 / comctl32

---
+++ VTK/Common/vtkTcl.h
@@ -0,0 +1,32 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkTcl.h,v $
+  Language:  C++
+  Date:      $Date: 2003-06-06 20:39:19 $
+  Version:   $Revision: 1.2 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __vtkTcl_h
+#define __vtkTcl_h
+
+#include "vtkToolkits.h"
+
+#ifdef TCL_TK_STATIC
+#ifndef STATIC_BUILD
+#define STATIC_BUILD
+#endif
+#endif
+
+#include <tcl.h>
+
+#endif
