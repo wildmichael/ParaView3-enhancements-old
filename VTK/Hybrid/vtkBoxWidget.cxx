541389753f023b9baae6b9928e0fb4db0a8302c2
cpbotha <cpbotha>
2003-07-02 13:38:46 +0000

    ERR: Removed two unreferenced local variables.

--- VTK/Hybrid/vtkBoxWidget.cxx
+++ VTK/Hybrid/vtkBoxWidget.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkBoxWidget.cxx,v $
   Language:  C++
-  Date:      $Date: 2003-07-02 13:02:10 $
-  Version:   $Revision: 1.35 $
+  Date:      $Date: 2003-07-02 13:38:46 $
+  Version:   $Revision: 1.36 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -37,7 +37,7 @@
 #include "vtkSphereSource.h"
 #include "vtkTransform.h"
 
-vtkCxxRevisionMacro(vtkBoxWidget, "$Revision: 1.35 $");
+vtkCxxRevisionMacro(vtkBoxWidget, "$Revision: 1.36 $");
 vtkStandardNewMacro(vtkBoxWidget);
 
 vtkBoxWidget::vtkBoxWidget()
@@ -1155,7 +1155,6 @@ void vtkBoxWidget::PlaceWidget(float bds[6])
 {
   int i;
   float bounds[6], center[3];
-  float *propcenter;
   
   this->AdjustBounds(bds,bounds,center);
   
@@ -1191,7 +1190,6 @@ void vtkBoxWidget::GetTransform(vtkTransform *t)
   double *p14 = pts + 3*14;
   double center[3], translate[3], scale[3], scaleVec[3][3];
   double InitialCenter[3];
-  float  position[3];
   int i;
 
   // The transformation is relative to the initial bounds.
