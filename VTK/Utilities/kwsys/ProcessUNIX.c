377f52993e33c997a3389f9c692c873eaec082d2
king <king>
2003-07-09 20:18:14 +0000

    ERR: Using strncpy instead of snprintf for portability.

--- VTK/Utilities/kwsys/ProcessUNIX.c
+++ VTK/Utilities/kwsys/ProcessUNIX.c
@@ -3,8 +3,8 @@
 Program:   KWSys - Kitware System Library
 Module:    $RCSfile: ProcessUNIX.c,v $
 Language:  C++
-Date:      $Date: 2003-07-07 13:12:34 $
-Version:   $Revision: 1.6 $
+Date:      $Date: 2003-07-09 20:18:14 $
+Version:   $Revision: 1.7 $
 
 Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
 See http://www.cmake.org/HTML/Copyright.html for details.
@@ -526,8 +526,7 @@ int kwsysProcess_WaitForData(kwsysProcess* cp, int pipes, char** data, int* leng
       {
       /* Select returned an error.  Leave the error description in the
          pipe buffer.  */
-      snprintf(cp->ErrorMessage, KWSYSPE_PIPE_BUFFER_SIZE,
-               "%s", strerror(errno));
+      strncpy(cp->ErrorMessage, strerror(errno), KWSYSPE_PIPE_BUFFER_SIZE);
       
       /* Kill the child now.  */
       kwsysProcess_Kill(cp);
@@ -730,7 +729,7 @@ static void kwsysProcessCleanup(kwsysProcess* cp, int error)
   /* If cleaning up due to an error, report the error message.  */
   if(error)
     {
-    snprintf(cp->ErrorMessage, KWSYSPE_PIPE_BUFFER_SIZE, "%s", strerror(errno));
+    strncpy(cp->ErrorMessage, strerror(errno), KWSYSPE_PIPE_BUFFER_SIZE);
     cp->State = kwsysProcess_State_Error;
     }
   
@@ -887,7 +886,7 @@ static void kwsysProcessChildErrorExit(kwsysProcess* cp)
 {
   /* Construct the error message.  */
   char buffer[KWSYSPE_PIPE_BUFFER_SIZE];
-  snprintf(buffer, KWSYSPE_PIPE_BUFFER_SIZE, "%s", strerror(errno));
+  strncpy(buffer, strerror(errno), KWSYSPE_PIPE_BUFFER_SIZE);
   
   /* Report the error to the parent through the special pipe.  */
   write(cp->PipeWriteEnds[KWSYSPE_PIPE_ERROR], buffer, strlen(buffer));
