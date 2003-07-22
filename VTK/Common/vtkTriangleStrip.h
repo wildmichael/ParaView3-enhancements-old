78ab82b0c6d57356ec46550e6570fbdd8c10d39e
will <will>
2003-07-22 17:04:30 +0000

    ENH: Method indicates whether cell is a primary or composite cell (i.e., whether it has fixed topology)

--- VTK/Common/vtkTriangleStrip.h
+++ VTK/Common/vtkTriangleStrip.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkTriangleStrip.h,v $
   Language:  C++
-  Date:      $Date: 2002-12-26 18:24:22 $
-  Version:   $Revision: 1.63 $
+  Date:      $Date: 2003-07-22 17:04:30 $
+  Version:   $Revision: 1.64 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -68,6 +68,7 @@ public:
   int Triangulate(int index, vtkIdList *ptIds, vtkPoints *pts);
   void Derivatives(int subId, float pcoords[3], float *values, 
                    int dim, float *derivs);
+  int IsPrimaryCell() {return 0;}
 
   // Description:
   // Return the center of the point cloud in parametric coordinates.
