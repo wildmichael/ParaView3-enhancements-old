88292ff2f0ffeaf18ba8d0a2fcd5158e0ce885b0
henderson <henderson>
2003-08-06 21:50:23 +0000

    using vtkstd::string instead of ostrstream

--- VTK/IO/vtkBYUWriter.cxx
+++ VTK/IO/vtkBYUWriter.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkBYUWriter.cxx,v $
   Language:  C++
-  Date:      $Date: 2003-08-06 21:24:45 $
-  Version:   $Revision: 1.52 $
+  Date:      $Date: 2003-08-06 21:50:23 $
+  Version:   $Revision: 1.53 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -27,7 +27,9 @@
 # include <unistd.h> /* unlink */
 #endif
 
-vtkCxxRevisionMacro(vtkBYUWriter, "$Revision: 1.52 $");
+#include <vtkstd/string>
+
+vtkCxxRevisionMacro(vtkBYUWriter, "$Revision: 1.53 $");
 vtkStandardNewMacro(vtkBYUWriter);
 
 // Create object so that it writes displacement, scalar, and texture files
@@ -117,44 +119,47 @@ void vtkBYUWriter::WriteData()
   this->WriteScalarFile(numPts);
   if (this->ErrorCode == vtkErrorCode::OutOfDiskSpaceError)
     {
-    ostrstream errorMessage;
+    vtkstd::string errorMessage;
     fclose(geomFp);
     unlink(this->GeometryFileName);
-    errorMessage << "Ran out of disk space; deleting files: "
-                 << this->GeometryFileName << " ";
+    errorMessage = "Ran out of disk space; deleting files: ";
+    errorMessage += this->GeometryFileName;
+    errorMessage += " ";
     if (this->DisplacementFileName)
       {
       unlink(this->DisplacementFileName);
-      errorMessage << this->DisplacementFileName << " ";
+      errorMessage += this->DisplacementFileName;
+      errorMessage += " ";
       }
     unlink(this->ScalarFileName);
-    errorMessage << this->ScalarFileName << ends;
-    vtkErrorMacro( << errorMessage.str());
-    errorMessage.rdbuf()->freeze(0);
+    errorMessage += this->ScalarFileName;
+    vtkErrorMacro( << errorMessage.c_str());
     return;
     }
   this->WriteTextureFile(numPts);
   if (this->ErrorCode == vtkErrorCode::OutOfDiskSpaceError)
     {
     fclose(geomFp);
-    ostrstream errorMessage;
+    vtkstd::string errorMessage;
     unlink(this->GeometryFileName);
-    errorMessage << "Ran out of disk space; deleting files: "
-                 << this->GeometryFileName << " ";
+    errorMessage = "Ran out of disk space; deleting files: ";
+    errorMessage += this->GeometryFileName;
+    errorMessage += " ";
     if (this->DisplacementFileName)
       {
       unlink(this->DisplacementFileName);
-      errorMessage << this->DisplacementFileName << " ";
+      errorMessage += this->DisplacementFileName;
+      errorMessage += " ";
       }
     if (this->ScalarFileName)
       {
       unlink(this->ScalarFileName);
-      errorMessage << this->ScalarFileName << " ";
+      errorMessage += this->ScalarFileName;
+      errorMessage += " ";
       }
     unlink(this->TextureFileName);
-    errorMessage << this->TextureFileName << ends;
-    vtkErrorMacro( << errorMessage.str());
-    errorMessage.rdbuf()->freeze(0);
+    errorMessage += this->TextureFileName;
+    vtkErrorMacro( << errorMessage.c_str());
     return;
     }
 
