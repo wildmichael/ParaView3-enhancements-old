819150623aa1fdcc114fa3daf350138a18bf2aa2
martink <martink>
2003-08-22 01:45:06 +0000

    Nightly Version

--- VTK/Common/vtkVersion.h
+++ VTK/Common/vtkVersion.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkVersion.h,v $
   Language:  C++
-  Date:      $Date: 2003-08-21 01:45:05 $
-  Version:   $Revision: 1.1669 $
+  Date:      $Date: 2003-08-22 01:45:06 $
+  Version:   $Revision: 1.1670 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -31,7 +31,7 @@
 
 #include "vtkObject.h"
 
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.1669 $, $Date: 2003-08-21 01:45:05 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.1670 $, $Date: 2003-08-22 01:45:06 $ (GMT)"
 
 
 class VTK_COMMON_EXPORT vtkVersion : public vtkObject
