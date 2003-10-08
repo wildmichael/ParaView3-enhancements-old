6e43617b42b372ce083a5cf74ef6c5b9049a9017
clarke <clarke>
2003-10-08 15:27:28 +0000

    Needed to include XdmfArray.h

--- Utilities/Xdmf/libsrc/XdmfNDGM.cxx
+++ Utilities/Xdmf/libsrc/XdmfNDGM.cxx
@@ -2,9 +2,9 @@
 /*                               XDMF                              */
 /*                   eXtensible Data Model and Format              */
 /*                                                                 */
-/*  Id : $Id: XdmfNDGM.cxx,v 1.3 2003-10-01 17:48:37 andy Exp $  */
-/*  Date : $Date: 2003-10-01 17:48:37 $ */
-/*  Version : $Revision: 1.3 $ */
+/*  Id : $Id: XdmfNDGM.cxx,v 1.4 2003-10-08 15:27:28 clarke Exp $  */
+/*  Date : $Date: 2003-10-08 15:27:28 $ */
+/*  Version : $Revision: 1.4 $ */
 /*                                                                 */
 /*  Author:                                                        */
 /*     Jerry A. Clarke                                             */
@@ -23,6 +23,7 @@
 /*                                                                 */
 /*******************************************************************/
 #include "XdmfNDGM.h"
+#include "XdmfArray.h"
 
 #ifndef WIN32
 #include <unistd.h>
