e9156c6fbcb0a5f2709ba7064c783e02a3a0ae8c
henderson <henderson>
2003-10-14 14:41:12 +0000

    making glyph UI prettier and less confusing

--- Servers/Filters/vtkPVGlyphFilter.h
+++ Servers/Filters/vtkPVGlyphFilter.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkPVGlyphFilter.h,v $
   Language:  C++
-  Date:      $Date: 2003-10-01 20:55:53 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2003-10-14 14:41:12 $
+  Version:   $Revision: 1.5 $
 
   Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
   All rights reserved.
@@ -31,27 +31,6 @@ public:
 
   // Description
   static vtkPVGlyphFilter *New();
-
-  // Description:
-  // If you want to use an arbitrary scalars array, then set its name here.
-  // By default this in NULL and the filter will use the active scalar array.
-  vtkGetStringMacro(InputScalarsSelection);
-  void SelectInputScalars(const char *fieldName) 
-    {this->SetInputScalarsSelection(fieldName);}
-
-  // Description:
-  // If you want to use an arbitrary vectors array, then set its name here.
-  // By default this in NULL and the filter will use the active vector array.
-  vtkGetStringMacro(InputVectorsSelection);
-  void SelectInputVectors(const char *fieldName) 
-    {this->SetInputVectorsSelection(fieldName);}
-
-  // Description:
-  // If you want to use an arbitrary normals array, then set its name here.
-  // By default this in NULL and the filter will use the active normal array.
-  vtkGetStringMacro(InputNormalsSelection);
-  void SelectInputNormals(const char *fieldName) 
-    {this->SetInputNormalsSelection(fieldName);}
 
   // Description:
   // Limit the number of points to glyph
