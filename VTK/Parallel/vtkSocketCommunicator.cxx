578252fc8c90e373c54c6e46d0c4acce7d34c5c6
andy <andy>
2003-10-17 11:12:07 +0000

    ERR: Windows has different interface

--- VTK/Parallel/vtkSocketCommunicator.cxx
+++ VTK/Parallel/vtkSocketCommunicator.cxx
@@ -3,8 +3,8 @@
 Program:   Visualization Toolkit
 Module:    $RCSfile: vtkSocketCommunicator.cxx,v $
 Language:  C++
-Date:      $Date: 2003-10-16 21:07:24 $
-Version:   $Revision: 1.42 $
+Date:      $Date: 2003-10-17 11:12:07 $
+Version:   $Revision: 1.43 $
 
 Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
 All rights reserved.
@@ -38,7 +38,7 @@ PURPOSE.  See the above copyright notice for more information.
 #define vtkCloseSocketMacro(sock) (close(sock))
 #endif
 
-vtkCxxRevisionMacro(vtkSocketCommunicator, "$Revision: 1.42 $");
+vtkCxxRevisionMacro(vtkSocketCommunicator, "$Revision: 1.43 $");
 vtkStandardNewMacro(vtkSocketCommunicator);
 
 //----------------------------------------------------------------------------
@@ -296,7 +296,11 @@ int vtkSocketCommunicator::WaitForConnection(int port)
   server.sin_port = htons(port);
   // Allow the socket to be bound to an address that is already in use
   int opt=1;
+#ifdef _WIN32
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char*) &opt, sizeof(int));
+#else
   setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void *) &opt, sizeof(int));
+#endif
 
   if ( bind(sock, (sockaddr *)&server, sizeof(server)) )
     {
