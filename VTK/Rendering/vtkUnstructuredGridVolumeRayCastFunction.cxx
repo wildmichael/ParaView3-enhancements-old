9d3e835dc8b77e8c84e740cdb3f9321c588f3755
avila <avila>
2003-11-24 20:05:46 +0000

    first check in of the unstructured grid volume rendering framework / ray casting algorithm

---
+++ VTK/Rendering/vtkUnstructuredGridVolumeRayCastFunction.cxx
@@ -0,0 +1,26 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkUnstructuredGridVolumeRayCastFunction.cxx,v $
+  Language:  C++
+  Date:      $Date: 2003-11-24 20:05:46 $
+  Version:   $Revision: 1.1 $
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkUnstructuredGridVolumeRayCastFunction.h"
+
+vtkCxxRevisionMacro(vtkUnstructuredGridVolumeRayCastFunction, "$Revision: 1.1 $");
+
+//----------------------------------------------------------------------------
+void vtkUnstructuredGridVolumeRayCastFunction::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
