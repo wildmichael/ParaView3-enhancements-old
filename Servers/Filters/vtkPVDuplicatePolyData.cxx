db256753ab95b51f2d06058815d347bade355ca2
berk <berk>
2004-01-20 21:06:01 +0000

    Fixed leak

--- Servers/Filters/vtkPVDuplicatePolyData.cxx
+++ Servers/Filters/vtkPVDuplicatePolyData.cxx
@@ -23,7 +23,7 @@
 #include "vtkSocketController.h"
 #include "vtkTiledDisplaySchedule.h"
 
-vtkCxxRevisionMacro(vtkPVDuplicatePolyData, "$Revision: 1.7 $");
+vtkCxxRevisionMacro(vtkPVDuplicatePolyData, "$Revision: 1.8 $");
 vtkStandardNewMacro(vtkPVDuplicatePolyData);
 
 vtkCxxSetObjectMacro(vtkPVDuplicatePolyData,Controller, vtkMultiProcessController);
@@ -53,6 +53,7 @@ vtkPVDuplicatePolyData::~vtkPVDuplicatePolyData()
     }
 
   this->SetController(0);
+  this->SetSocketController(0);
 }
 
 
