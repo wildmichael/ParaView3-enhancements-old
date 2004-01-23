9a60e5ca43e48f2f675d96de5554965ba56ca0a0
martink <martink>
2004-01-23 17:54:47 +0000

    fix FMM memory error

--- VTK/IO/vtkXMLWriter.cxx
+++ VTK/IO/vtkXMLWriter.cxx
@@ -33,7 +33,7 @@
 # include <io.h> /* unlink */
 #endif
 
-vtkCxxRevisionMacro(vtkXMLWriter, "$Revision: 1.31 $");
+vtkCxxRevisionMacro(vtkXMLWriter, "$Revision: 1.32 $");
 vtkCxxSetObjectMacro(vtkXMLWriter, Compressor, vtkDataCompressor);
 
 //----------------------------------------------------------------------------
@@ -2124,7 +2124,7 @@ void vtkXMLWriter::DestroyStringArray(int numStrings, char** strings)
       delete [] strings[i];
       }
     }
-  delete strings;
+  delete [] strings;
 }
 
 //----------------------------------------------------------------------------
