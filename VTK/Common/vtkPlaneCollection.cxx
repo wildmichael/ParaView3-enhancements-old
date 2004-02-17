688732b9d98a61d5439d77305e106e7431b1b0a3
martink <martink>
2004-02-17 20:48:56 +0000

    fix collections some

--- VTK/Common/vtkPlaneCollection.cxx
+++ VTK/Common/vtkPlaneCollection.cxx
@@ -15,5 +15,11 @@
 #include "vtkPlaneCollection.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkPlaneCollection, "$Revision: 1.7 $");
+vtkCxxRevisionMacro(vtkPlaneCollection, "$Revision: 1.8 $");
 vtkStandardNewMacro(vtkPlaneCollection);
+
+vtkPlane *vtkPlaneCollection::GetNextPlane(
+  vtkCollectionSimpleIterator &cookie) 
+{
+  return static_cast<vtkPlane *>(this->GetNextItemAsObject(cookie));
+}
