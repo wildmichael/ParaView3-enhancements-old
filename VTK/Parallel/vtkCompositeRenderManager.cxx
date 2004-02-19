40728e964b58f1ea7813c97a37cf83152799238a
martink <martink>
2004-02-19 15:08:36 +0000

    some collection cleanup

--- VTK/Parallel/vtkCompositeRenderManager.cxx
+++ VTK/Parallel/vtkCompositeRenderManager.cxx
@@ -20,12 +20,11 @@
 #include "vtkMultiProcessController.h"
 #include "vtkObjectFactory.h"
 #include "vtkRenderer.h"
-#include "vtkRendererCollection.h"
 #include "vtkRenderWindow.h"
 #include "vtkTimerLog.h"
 #include "vtkUnsignedCharArray.h"
 
-vtkCxxRevisionMacro(vtkCompositeRenderManager, "$Revision: 1.3 $");
+vtkCxxRevisionMacro(vtkCompositeRenderManager, "$Revision: 1.4 $");
 vtkStandardNewMacro(vtkCompositeRenderManager);
 
 vtkCxxSetObjectMacro(vtkCompositeRenderManager, Compositer, vtkCompositer);
