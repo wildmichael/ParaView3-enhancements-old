175e17f2a5907ff0fd93ab78899a59ae1cee1f18
malaterre <malaterre>
2004-05-03 15:04:10 +0000

    BUG: Remove PrintSelf of vtkDebugLeaks

--- VTK/Common/vtkDebugLeaks.cxx
+++ VTK/Common/vtkDebugLeaks.cxx
@@ -36,7 +36,7 @@ int vtkDebugLeaksIgnoreClassesCheck(const char* s)
   return 0;
 }
 
-vtkCxxRevisionMacro(vtkDebugLeaks, "$Revision: 1.27 $");
+vtkCxxRevisionMacro(vtkDebugLeaks, "$Revision: 1.28 $");
 vtkStandardNewMacro(vtkDebugLeaks);
 
 //----------------------------------------------------------------------------
@@ -386,17 +386,6 @@ void vtkDebugLeaks::ClassFinalize()
   delete vtkDebugLeaks::CriticalSection;
   vtkDebugLeaks::CriticalSection = 0;
 #endif
-}
-
-//----------------------------------------------------------------------------
-void vtkDebugLeaks::PrintSelf(ostream& os, vtkIndent indent)
-{
-  this->Superclass::PrintSelf(os,indent);
-  
-  os << indent << "MemoryTable:\n";
-  this->MemoryTable->PrintTable(os);
-
-  os << indent << "CriticalSection: " << this->CriticalSection << "\n";
 }
 
 //----------------------------------------------------------------------------
