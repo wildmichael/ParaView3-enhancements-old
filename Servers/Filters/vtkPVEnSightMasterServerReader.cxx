65f44081784faf243fe51bc9b09f58f87cdd05a7
dcthomp <dcthomp>
2004-05-05 05:48:44 +0000

    * ERR: Fix compile warning/error: Redeclaration of "int i" unneccessary.

--- Servers/Filters/vtkPVEnSightMasterServerReader.cxx
+++ Servers/Filters/vtkPVEnSightMasterServerReader.cxx
@@ -41,7 +41,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVEnSightMasterServerReader);
-vtkCxxRevisionMacro(vtkPVEnSightMasterServerReader, "$Revision: 1.21 $");
+vtkCxxRevisionMacro(vtkPVEnSightMasterServerReader, "$Revision: 1.22 $");
 
 vtkCxxSetObjectMacro(vtkPVEnSightMasterServerReader, Controller,
                      vtkMultiProcessController);
@@ -296,7 +296,7 @@ void vtkPVEnSightMasterServerReader::ExecuteInformation()
     this->InformationError = 1;
     return;
     }
-  for (int i= 0;i < this->NumberOfOutputs; ++i)
+  for (i=0; i < this->NumberOfOutputs; ++i)
     {
     this->Outputs[i]->SetMaximumNumberOfPieces(-1);
     }
