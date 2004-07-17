03e91c7eef69616fd4e6ca9262feebc21c543287
martink <martink>
2004-07-17 14:16:00 +0000

    fix warnings

--- VTK/Common/vtkProcessObject.cxx
+++ VTK/Common/vtkProcessObject.cxx
@@ -27,7 +27,7 @@
 
 #include "vtkDebugLeaks.h"
 
-vtkCxxRevisionMacro(vtkProcessObject, "$Revision: 1.41 $");
+vtkCxxRevisionMacro(vtkProcessObject, "$Revision: 1.42 $");
 
 //----------------------------------------------------------------------------
 
@@ -44,6 +44,9 @@ public:
 #endif
     return new vtkProcessObjectDummyData;
     }
+private:
+  vtkProcessObjectDummyData(const vtkProcessObjectDummyData&);
+  void operator=(const vtkProcessObjectDummyData&);  // Not implemented.
 };
 
 //----------------------------------------------------------------------------
