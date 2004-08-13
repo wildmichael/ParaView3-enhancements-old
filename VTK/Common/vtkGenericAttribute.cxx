8badf51a4b5ac5681678152be3f0472b18de485f
andy <andy>
2004-08-13 13:09:09 +0000

    ERR: Some build fixes

--- VTK/Common/vtkGenericAttribute.cxx
+++ VTK/Common/vtkGenericAttribute.cxx
@@ -18,8 +18,19 @@
 #include "vtkGenericAttribute.h"
 #include <assert.h>
 
-vtkCxxRevisionMacro(vtkGenericAttribute, "$Revision: 1.1 $");
+vtkCxxRevisionMacro(vtkGenericAttribute, "$Revision: 1.2 $");
 
+//---------------------------------------------------------------------------
+vtkGenericAttribute::vtkGenericAttribute()
+{
+}
+
+//---------------------------------------------------------------------------
+vtkGenericAttribute::~vtkGenericAttribute()
+{
+}
+
+//---------------------------------------------------------------------------
 void vtkGenericAttribute::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
