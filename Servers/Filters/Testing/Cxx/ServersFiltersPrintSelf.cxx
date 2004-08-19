a74cae5973e075547fc843b9aa4cc580642afb87
malaterre <malaterre>
2004-08-19 18:24:54 +0000

    ENH: Moved around test.cxx

--- Servers/Filters/Testing/Cxx/ServersFiltersPrintSelf.cxx
+++ Servers/Filters/Testing/Cxx/ServersFiltersPrintSelf.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   ParaView
-  Module:    $RCSfile: test.cxx,v $
+  Module:    $RCSfile: ServersFiltersPrintSelf.cxx,v $
 
   Copyright (c) Kitware, Inc.
   All rights reserved.
