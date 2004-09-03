6ee9c2ce1f7c99657c98d55db4690296300d3348
henderson <henderson>
2004-09-03 14:16:51 +0000

    These set methods in the TempTessellator filter used to be doing more than just what happens in the vtkCxxSetObjectMacro; the filter was crashing because the tessellator's algorithm was not set.

--- Servers/Filters/vtkTempTessellatorFilter.cxx
+++ Servers/Filters/vtkTempTessellatorFilter.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkTempTessellatorFilter.cxx,v $
   Language:  C++
-  Date:      $Date: 2004-08-24 14:50:23 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 2004-09-03 14:16:51 $
+  Version:   $Revision: 1.8 $
 
   Copyright 2003 Sandia Corporation.
   Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
@@ -32,7 +32,7 @@
 #include "vtkDataSetSubdivisionAlgorithm.h"
 #include "vtkSubdivisionAlgorithm.h"
 
-vtkCxxRevisionMacro(vtkTempTessellatorFilter, "$Revision: 1.7 $");
+vtkCxxRevisionMacro(vtkTempTessellatorFilter, "$Revision: 1.8 $");
 vtkStandardNewMacro(vtkTempTessellatorFilter);
 
 // ========================================
@@ -189,9 +189,6 @@ void vtkTempTessellatorFilter::OutputLine( const double* a, const double* b )
 
 // ========================================
 
-vtkCxxSetObjectMacro(vtkTempTessellatorFilter, Tessellator, vtkStreamingTessellator);
-vtkCxxSetObjectMacro(vtkTempTessellatorFilter, Subdivider, vtkDataSetSubdivisionAlgorithm);
-
 // constructor/boilerplate members
 vtkTempTessellatorFilter::vtkTempTessellatorFilter()
   : Tessellator( 0 ), Subdivider( 0 )
@@ -243,6 +240,56 @@ unsigned long vtkTempTessellatorFilter::GetMTime()
 
   return mt;
 }
+
+void vtkTempTessellatorFilter::SetTessellator( vtkStreamingTessellator* t )
+{
+  if ( this->Tessellator == t )
+    {
+    return;
+    }
+  
+  if ( this->Tessellator )
+    {
+    this->Tessellator->UnRegister( this );
+    }
+  
+  this->Tessellator = t; 
+  
+  if ( this->Tessellator )
+    {
+    this->Tessellator->Register( this );
+    this->Tessellator->SetSubdivisionAlgorithm( this->Subdivider );
+    }
+  
+  this->Modified();
+}
+
+void vtkTempTessellatorFilter::SetSubdivider( vtkDataSetSubdivisionAlgorithm* s )
+{
+  if ( this->Subdivider == s )
+    {
+    return;
+    }
+  
+  if ( this->Subdivider )
+    {
+    this->Subdivider->UnRegister( this );
+    }
+  
+  this->Subdivider = s;
+  
+  if ( this->Subdivider )
+    {
+    this->Subdivider->Register( this );
+    }
+  
+  if ( this->Tessellator )
+    {
+    this->Tessellator->SetSubdivisionAlgorithm( this->Subdivider );
+    }
+  
+  this->Modified();
+} 
 
 void vtkTempTessellatorFilter::SetFieldCriterion( int s, double err )
 {
