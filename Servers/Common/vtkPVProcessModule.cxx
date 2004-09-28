a475fe3c6de5fb789b6a3a29f9941ebf56b77eff
andy <andy>
2004-09-28 21:16:53 +0000

    ENH: Add ApplicationInstallationDirectory ivar. Needs to be set

--- Servers/Common/vtkPVProcessModule.cxx
+++ Servers/Common/vtkPVProcessModule.cxx
@@ -53,7 +53,7 @@ int vtkPVProcessModule::GlobalLODFlag = 0;
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVProcessModule);
-vtkCxxRevisionMacro(vtkPVProcessModule, "$Revision: 1.20 $");
+vtkCxxRevisionMacro(vtkPVProcessModule, "$Revision: 1.21 $");
 
 //----------------------------------------------------------------------------
 vtkPVProcessModule::vtkPVProcessModule()
@@ -65,12 +65,14 @@ vtkPVProcessModule::vtkPVProcessModule()
   this->UseTriangleStrips = 0;
   this->UseImmediateMode = 1;
   this->Options = 0;
+  this->ApplicationInstallationDirectory = 0;
 }
 
 //----------------------------------------------------------------------------
 vtkPVProcessModule::~vtkPVProcessModule()
 { 
   this->SetDemoPath(0);
+  this->SetApplicationInstallationDirectory(0);
   this->FinalizeInterpreter();
   this->ServerInformation->Delete();
 }
