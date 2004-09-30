b8f38bfe10a90f7f7be081b5f9433a830dbdc850
martink <martink>
2004-09-30 18:58:11 +0000

    ENH: some cleanup to require exact extent

--- VTK/Graphics/vtkContourFilter.cxx
+++ VTK/Graphics/vtkContourFilter.cxx
@@ -31,7 +31,7 @@
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkContourFilter, "$Revision: 1.104 $");
+vtkCxxRevisionMacro(vtkContourFilter, "$Revision: 1.105 $");
 vtkStandardNewMacro(vtkContourFilter);
 vtkCxxSetObjectMacro(vtkContourFilter,ScalarTree,vtkScalarTree);
 
@@ -110,7 +110,6 @@ void vtkContourFilter::Execute()
   vtkDataArray *cellScalars;
 
   vtkDebugMacro(<< "Executing contour filter");
-  input->Crop();
   if (input->GetDataObjectType() == VTK_UNSTRUCTURED_GRID)
     {
     vtkDebugMacro(<< "Processing unstructured grid");
