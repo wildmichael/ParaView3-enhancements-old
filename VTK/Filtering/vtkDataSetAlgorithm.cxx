d956ca65e410f715b4d895598e83a4a28cd3f643
berk <berk>
2004-10-05 18:30:29 +0000

    ENH: Removed unused header file

--- VTK/Filtering/vtkDataSetAlgorithm.cxx
+++ VTK/Filtering/vtkDataSetAlgorithm.cxx
@@ -25,10 +25,9 @@
 #include "vtkStreamingDemandDrivenPipeline.h"
 #include "vtkStructuredGrid.h"
 #include "vtkStructuredPoints.h"
-#include "vtkTrivialProducer.h"
 #include "vtkUnstructuredGrid.h"
 
-vtkCxxRevisionMacro(vtkDataSetAlgorithm, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkDataSetAlgorithm, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkDataSetAlgorithm);
 
 //----------------------------------------------------------------------------
