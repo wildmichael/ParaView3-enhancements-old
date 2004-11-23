d13b936d0e0e7436a0152366af0edbaf288282b8
henderson <henderson>
2004-11-23 14:12:44 +0000

    ENH: For consistency, rename ExecuteInformation to RequestInformation

--- VTK/Graphics/vtkConeSource.cxx
+++ VTK/Graphics/vtkConeSource.cxx
@@ -26,7 +26,7 @@
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkConeSource, "$Revision: 1.68 $");
+vtkCxxRevisionMacro(vtkConeSource, "$Revision: 1.69 $");
 vtkStandardNewMacro(vtkConeSource);
 
 //----------------------------------------------------------------------------
@@ -279,7 +279,7 @@ int vtkConeSource::RequestData(
 }
 
 //----------------------------------------------------------------------------
-int vtkConeSource::ExecuteInformation(
+int vtkConeSource::RequestInformation(
   vtkInformation *vtkNotUsed(request),
   vtkInformationVector **vtkNotUsed(inputVector),
   vtkInformationVector *outputVector)
