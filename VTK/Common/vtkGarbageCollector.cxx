ca88025790594eb818d51a5a52650644e5138736
king <king>
2004-12-01 12:08:09 +0000

    COMP: Remove void return.  It does not work on all compilers.

--- VTK/Common/vtkGarbageCollector.cxx
+++ VTK/Common/vtkGarbageCollector.cxx
@@ -25,7 +25,7 @@
 
 #include <assert.h>
 
-vtkCxxRevisionMacro(vtkGarbageCollector, "$Revision: 1.19 $");
+vtkCxxRevisionMacro(vtkGarbageCollector, "$Revision: 1.20 $");
 
 class vtkGarbageCollectorSingleton;
 
@@ -850,7 +850,7 @@ void vtkGarbageCollector::DeferredCollectionPush()
   // Forward the call to the singleton.
   if(vtkGarbageCollectorSingletonInstance)
     {
-    return vtkGarbageCollectorSingletonInstance->DeferredCollectionPush();
+    vtkGarbageCollectorSingletonInstance->DeferredCollectionPush();
     }
 }
 
