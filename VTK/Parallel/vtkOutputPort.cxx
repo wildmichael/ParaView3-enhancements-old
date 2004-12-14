14325e445a5427b45bf214d68e7107552700de96
martink <martink>
2004-12-14 15:28:49 +0000

    COMP: fix compiler warning

--- VTK/Parallel/vtkOutputPort.cxx
+++ VTK/Parallel/vtkOutputPort.cxx
@@ -23,7 +23,7 @@
 #include "vtkMultiProcessController.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkOutputPort, "$Revision: 1.11 $");
+vtkCxxRevisionMacro(vtkOutputPort, "$Revision: 1.12 $");
 vtkStandardNewMacro(vtkOutputPort);
 
 vtkCxxSetObjectMacro(vtkOutputPort,Controller,vtkMultiProcessController);
@@ -338,7 +338,7 @@ void vtkOutputPort::WaitForUpdate()
 
 //----------------------------------------------------------------------------
 int vtkOutputPort
-::FillInputPortInformation(int port, vtkInformation* info)
+::FillInputPortInformation(int vtkNotUsed( port ), vtkInformation* info)
 {
   info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
   return 1;
