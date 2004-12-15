aa8a598c71177a5bc8283da5c7948f84ca82c4fc
martink <martink>
2004-12-15 16:45:32 +0000

    ENH: now includes vtkWindows.h as required

--- Common/KWCommon/vtkKWProcessStatistics.cxx
+++ Common/KWCommon/vtkKWProcessStatistics.cxx
@@ -13,7 +13,7 @@
 =========================================================================*/
 #include "vtkKWProcessStatistics.h"
 
-vtkCxxRevisionMacro(vtkKWProcessStatistics, "$Revision: 1.7 $");
+vtkCxxRevisionMacro(vtkKWProcessStatistics, "$Revision: 1.8 $");
 
 #ifdef __linux
 #include <sys/procfs.h>
@@ -29,6 +29,7 @@ vtkCxxRevisionMacro(vtkKWProcessStatistics, "$Revision: 1.7 $");
 #endif
 
 #include "vtkObjectFactory.h"
+#include "vtkWindows.h"
 
 vtkStandardNewMacro(vtkKWProcessStatistics);
 
