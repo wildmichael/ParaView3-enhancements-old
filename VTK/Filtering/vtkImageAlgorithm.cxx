8d0e64d17498a19f693a509bbb338548b33b6088
king <king>
2004-12-17 15:07:56 +0000

    STYLE: Removed unused vtkImageAlgorithmToDataSetFriendship class.

--- VTK/Filtering/vtkImageAlgorithm.cxx
+++ VTK/Filtering/vtkImageAlgorithm.cxx
@@ -24,16 +24,7 @@
 #include "vtkPointData.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 
-vtkCxxRevisionMacro(vtkImageAlgorithm, "$Revision: 1.16 $");
-
-class vtkImageAlgorithmToDataSetFriendship
-{
-public:
-  static void GenerateGhostLevelArray(vtkDataSet* ds)
-    {
-    ds->GenerateGhostLevelArray();
-    }
-};
+vtkCxxRevisionMacro(vtkImageAlgorithm, "$Revision: 1.17 $");
 
 //----------------------------------------------------------------------------
 vtkImageAlgorithm::vtkImageAlgorithm()
