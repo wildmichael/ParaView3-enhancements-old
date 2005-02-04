65a3513955a32dff05021c94c266ffaa754a1d0c
malaterre <malaterre>
2005-02-04 20:12:52 +0000

    ENH: Change the vtkErrorMacro commented out with vtkDebugMacro.

--- VTK/Graphics/vtkAppendFilter.cxx
+++ VTK/Graphics/vtkAppendFilter.cxx
@@ -25,7 +25,7 @@
 #include "vtkPointData.h"
 #include "vtkUnstructuredGrid.h"
 
-vtkCxxRevisionMacro(vtkAppendFilter, "$Revision: 1.72 $");
+vtkCxxRevisionMacro(vtkAppendFilter, "$Revision: 1.73 $");
 vtkStandardNewMacro(vtkAppendFilter);
 
 //----------------------------------------------------------------------------
@@ -180,7 +180,7 @@ int vtkAppendFilter::RequestData(
 
   if ( numPts < 1)
     {
-    //vtkErrorMacro(<<"No data to append!");
+    vtkDebugMacro(<<"No data to append!");
     return 1;
     }
   
