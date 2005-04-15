9be3bcb049530546f78d66518d1a36e95b8938db
utkarsh <utkarsh>
2005-04-15 19:18:39 +0000

    BUG: erronoesly changed the print state for RepeatCommand

--- Servers/ServerManager/vtkSMVectorProperty.cxx
+++ Servers/ServerManager/vtkSMVectorProperty.cxx
@@ -16,7 +16,7 @@
 
 #include "vtkPVXMLElement.h"
 
-vtkCxxRevisionMacro(vtkSMVectorProperty, "$Revision: 1.5 $");
+vtkCxxRevisionMacro(vtkSMVectorProperty, "$Revision: 1.6 $");
 
 //---------------------------------------------------------------------------
 vtkSMVectorProperty::vtkSMVectorProperty()
@@ -96,8 +96,7 @@ void vtkSMVectorProperty::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "NumberOfElements: " << this->GetNumberOfElements() << endl;
   os << indent << "NumberOfElementsPerCommand: " 
      << this->GetNumberOfElementsPerCommand() << endl;
-  os << indent << "RepeatCommand: " << 
-    (this->RepeatCommand? this->RepeatCommand : "(null)" ) << endl;
+  os << indent << "RepeatCommand: " << this->RepeatCommand << endl;
   os << indent << "CleanCommand: " << 
     (this->CleanCommand? this->CleanCommand : "(null)" ) << endl;
   os << indent << "UseIndex: " << this->UseIndex << endl;
