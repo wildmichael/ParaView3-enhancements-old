c8ac07933ca75bedbebe108764cbafd09ce9cdb9
malaterre <malaterre>
2005-04-28 14:39:23 +0000

    ENH: Fix initialization of NumberOfCells + minor cells

--- VTK/IO/vtkXMLUnstructuredDataReader.cxx
+++ VTK/IO/vtkXMLUnstructuredDataReader.cxx
@@ -23,7 +23,7 @@
 #include "vtkInformation.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 
-vtkCxxRevisionMacro(vtkXMLUnstructuredDataReader, "$Revision: 1.18 $");
+vtkCxxRevisionMacro(vtkXMLUnstructuredDataReader, "$Revision: 1.19 $");
 
 //----------------------------------------------------------------------------
 vtkXMLUnstructuredDataReader::vtkXMLUnstructuredDataReader()
@@ -37,7 +37,10 @@ vtkXMLUnstructuredDataReader::vtkXMLUnstructuredDataReader()
 //----------------------------------------------------------------------------
 vtkXMLUnstructuredDataReader::~vtkXMLUnstructuredDataReader()
 {
-  if(this->NumberOfPieces) { this->DestroyPieces(); }
+  if(this->NumberOfPieces)
+    {
+    this->DestroyPieces();
+    }
 }
 
 //----------------------------------------------------------------------------
