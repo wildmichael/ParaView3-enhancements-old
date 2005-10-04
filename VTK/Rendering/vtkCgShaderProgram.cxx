db1a47edab1142054942e146895841c22ec5ac09
utkarsh <utkarsh>
2005-10-04 17:51:38 +0000

    STYLE: fixed copyrights

--- VTK/Rendering/vtkCgShaderProgram.cxx
+++ VTK/Rendering/vtkCgShaderProgram.cxx
@@ -2,18 +2,24 @@
 
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkCgShaderProgram.cxx,v $
-  Language:  C++
-  Date:      $Date: 2005-10-04 15:25:52 $
-  Version:   $Revision: 1.2 $
 
-  Copyright 2003 Sandia Corporation.
-  Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
-  license for use of this work by or on behalf of the
-  U.S. Government. Redistribution and use in source and binary forms, with
-  or without modification, are permitted provided that this Notice and any
-  statement of authorship are reproduced on all copies.
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
+/*
+ * Copyright 2003 Sandia Corporation.
+ * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+ * license for use of this work by or on behalf of the
+ * U.S. Government. Redistribution and use in source and binary forms, with
+ * or without modification, are permitted provided that this Notice and any
+ * statement of authorship are reproduced on all copies.
+ */
 
 #include "vtkCgShaderProgram.h"
 #include "vtkCgShader.h"
@@ -25,7 +31,7 @@
 #include <vtkObjectFactory.h>
 
 
-vtkCxxRevisionMacro(vtkCgShaderProgram, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkCgShaderProgram, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkCgShaderProgram);
 
 //----------------------------------------------------------------------------
