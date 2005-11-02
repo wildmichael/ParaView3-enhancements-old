dad8ef680834566292c75712cc3443da1990de1f
yogi.girdhar <yogi.girdhar>
2005-11-02 20:35:39 +0000

    BUG: Arguments to the shader are now updated every time the camera moves

--- VTK/Rendering/vtkShader.cxx
+++ VTK/Rendering/vtkShader.cxx
@@ -339,7 +339,7 @@ public:
 };
 
 //-----------------------------------------------------------------------------
-vtkCxxRevisionMacro(vtkShader, "$Revision: 1.16 $")
+vtkCxxRevisionMacro(vtkShader, "$Revision: 1.17 $")
 vtkCxxSetObjectMacro(vtkShader, XMLShader, vtkXMLShader);
 //-----------------------------------------------------------------------------
 vtkShader::vtkShader()
@@ -659,7 +659,8 @@ void vtkShader::SetCameraParameter(vtkActor* , vtkRenderer* ren,
   vtkXMLDataElement* elem)
 {
   vtkCamera* camera = ren->GetActiveCamera();
-  if (this->GetMTime() < this->PassShaderVariablesTime)
+  if (this->GetMTime() < this->PassShaderVariablesTime &&
+    camera->GetMTime() < this->PassShaderVariablesTime)
     {
     return; // no need to update.
     }
