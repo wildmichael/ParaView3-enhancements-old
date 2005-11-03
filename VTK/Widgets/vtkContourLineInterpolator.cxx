5381ea6ceb7ce022d840af35d4e071861bee0201
will <will>
2005-11-03 23:17:18 +0000

    ENH:Beginning merge process from Widgets-Redesign VTK branch

---
+++ VTK/Widgets/vtkContourLineInterpolator.cxx
@@ -0,0 +1,33 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkContourLineInterpolator.cxx,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkContourLineInterpolator.h"
+
+vtkCxxRevisionMacro(vtkContourLineInterpolator, "$Revision: 1.2 $");
+
+//----------------------------------------------------------------------
+vtkContourLineInterpolator::vtkContourLineInterpolator()
+{
+}
+
+//----------------------------------------------------------------------
+vtkContourLineInterpolator::~vtkContourLineInterpolator()
+{
+}
+
+//----------------------------------------------------------------------
+void vtkContourLineInterpolator::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);  
+}
