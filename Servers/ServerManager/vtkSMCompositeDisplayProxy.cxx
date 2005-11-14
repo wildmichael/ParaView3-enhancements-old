153d4d0276e2adcde61e828303c09da385237118
utkarsh <utkarsh>
2005-11-14 18:33:23 +0000

    ENH: 1) Now vtkSMProxyManager::UpdateRegisteredProxies() can check if a proxy is modified at all before call UpdateVTKObjects() on it. 2) Spilt vtkSMProxy::UpdateInformation() into two separate parts:   * UpdatePropertyInformation()-- updates info properties and dependent domains.   * UpdatePipelineInformation()-- calls UpdateInformation on vtkAlgorithm object.   vtkSMProxy::MarkModified() always updates all properties (i.e. UpdatePropertyInformation()). Any filter providing information through info properties that depends on the execution of the pipeline will not work -- this as such is a bad filter design. For all other proxies, info properties will be updated automatically. Ofcourse there is a hit with the info properties being update a bit too many times -- but it will help in long run.
    BUG: vtkSMProxy would think properties are modified even when a information property value changed. This behaviour is not appropriate. Fixed that.

--- Servers/ServerManager/vtkSMCompositeDisplayProxy.cxx
+++ Servers/ServerManager/vtkSMCompositeDisplayProxy.cxx
@@ -29,7 +29,7 @@
 //-----------------------------------------------------------------------------
 
 vtkStandardNewMacro(vtkSMCompositeDisplayProxy);
-vtkCxxRevisionMacro(vtkSMCompositeDisplayProxy, "$Revision: 1.9 $");
+vtkCxxRevisionMacro(vtkSMCompositeDisplayProxy, "$Revision: 1.10 $");
 //-----------------------------------------------------------------------------
 vtkSMCompositeDisplayProxy::vtkSMCompositeDisplayProxy()
 {
@@ -75,10 +75,11 @@ vtkSMCompositeDisplayProxy::~vtkSMCompositeDisplayProxy()
 //-----------------------------------------------------------------------------
 void vtkSMCompositeDisplayProxy::CreateVTKObjects(int numObjects)
 {
-  if (this->ObjectsCreated)
+  if (this->ObjectsCreated || !this->CanCreateProxy)
     {
     return;
     }
+
   this->CollectProxy = this->GetSubProxy("Collect");
   this->LODCollectProxy = this->GetSubProxy("LODCollect");
 
