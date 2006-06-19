adc9fded6414aa2c8bd0324d9afd305ce302810a
utkarsh <utkarsh>
2006-06-19 17:58:05 +0000

    ENH: When camera properties are synchronized, the property behaves as if the values have been pushed to the server.

--- Servers/ServerManager/vtkSMDoubleVectorProperty.cxx
+++ Servers/ServerManager/vtkSMDoubleVectorProperty.cxx
@@ -22,7 +22,7 @@
 #include <vtkstd/vector>
 
 vtkStandardNewMacro(vtkSMDoubleVectorProperty);
-vtkCxxRevisionMacro(vtkSMDoubleVectorProperty, "$Revision: 1.31 $");
+vtkCxxRevisionMacro(vtkSMDoubleVectorProperty, "$Revision: 1.32 $");
 
 struct vtkSMDoubleVectorPropertyInternals
 {
@@ -52,6 +52,12 @@ vtkSMDoubleVectorProperty::~vtkSMDoubleVectorProperty()
 {
   delete this->Internals;
   this->SetSetNumberCommand(0);
+}
+
+//---------------------------------------------------------------------------
+void vtkSMDoubleVectorProperty::UpdateLastPushedValues()
+{
+  this->Internals->UpdateLastPushedValues();
 }
 
 //---------------------------------------------------------------------------
