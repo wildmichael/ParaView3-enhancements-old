39190d76370d4265d79642dfcf610fdbbbd7f542
will <will>
2006-06-21 10:43:54 +0000

    ENH:Cleaned up code

--- VTK/Widgets/vtkLineWidget.cxx
+++ VTK/Widgets/vtkLineWidget.cxx
@@ -35,7 +35,7 @@
 #include "vtkRenderer.h"
 #include "vtkSphereSource.h"
 
-vtkCxxRevisionMacro(vtkLineWidget, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkLineWidget, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkLineWidget);
 
 //----------------------------------------------------------------------------
@@ -199,6 +199,7 @@ vtkLineWidget::vtkLineWidget()
   this->CurrentPointWidget = NULL;
 }
 
+//----------------------------------------------------------------------------
 vtkLineWidget::~vtkLineWidget()
 {
   this->LineActor->Delete();
