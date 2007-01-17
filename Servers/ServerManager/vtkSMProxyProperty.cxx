8850a88129945166d39ccdee6389055e87daf582
utkarsh <utkarsh>
2007-01-17 16:24:12 +0000

    ENH: BUG #4153: Adding Cache check box.

--- Servers/ServerManager/vtkSMProxyProperty.cxx
+++ Servers/ServerManager/vtkSMProxyProperty.cxx
@@ -32,7 +32,7 @@
 #include "vtkStdString.h"
 
 vtkStandardNewMacro(vtkSMProxyProperty);
-vtkCxxRevisionMacro(vtkSMProxyProperty, "$Revision: 1.39 $");
+vtkCxxRevisionMacro(vtkSMProxyProperty, "$Revision: 1.40 $");
 
 struct vtkSMProxyPropertyInternals
 {
@@ -577,7 +577,6 @@ void vtkSMProxyProperty::ChildSaveState(vtkPVXMLElement* propertyElement,
         }
       }
     propertyElement->AddNestedElement(element);
-    propertyElement->AddAttribute("clear",  "0");
     element->Delete();
     }
 }
