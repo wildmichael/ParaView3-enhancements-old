a5aa74e8bfdeafd6056f40ba2d7514112148caf2
dave.demarle <dave.demarle>
2007-03-14 18:08:03 +0000

    BUG: Get rid of false positive results when extracting 2D cells by location. ENH: Put selection of points by id in. This includes using globalids attribute or a named array as the ids. STYLE: Make extract filters all take dataset on port 0 and selection on port 1. STYLE: Clean up vtkSelection::CONTENT_TYPE all id selections now simply called ID. vtkSelection::FIELD_TYPE decides POINT or CELL. Rename content type POINTS to LOCATIONS. Because of that, rename vtkExtractSelectedPoints to vtkExtractSelectedLocations.

---
+++ VTK/Graphics/vtkExtractSelectedLocations.cxx
@@ -0,0 +1,198 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkExtractSelectedLocations.cxx,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkExtractSelectedLocations.h"
+
+#include "vtkDataSet.h"
+#include "vtkPoints.h"
+#include "vtkIdList.h"
+#include "vtkIdTypeArray.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtkSelection.h"
+#include "vtkUnstructuredGrid.h"
+#include "vtkPointData.h"
+#include "vtkCellData.h"
+#include "vtkDoubleArray.h"
+#include "vtkCell.h"
+
+vtkCxxRevisionMacro(vtkExtractSelectedLocations, "$Revision: 1.1 $");
+vtkStandardNewMacro(vtkExtractSelectedLocations);
+
+//----------------------------------------------------------------------------
+vtkExtractSelectedLocations::vtkExtractSelectedLocations()
+{
+  this->SetNumberOfInputPorts(2);
+}
+
+//----------------------------------------------------------------------------
+vtkExtractSelectedLocations::~vtkExtractSelectedLocations()
+{
+}
+
+//----------------------------------------------------------------------------
+int vtkExtractSelectedLocations::RequestData(
+  vtkInformation *vtkNotUsed(request),
+  vtkInformationVector **inputVector,
+  vtkInformationVector *outputVector)
+{
+  // get the info objects
+  vtkInformation *selInfo = inputVector[1]->GetInformationObject(0);
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+
+  // get the selection, input and ouptut
+  vtkDataSet *input = vtkDataSet::SafeDownCast(
+    inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkUnstructuredGrid *output = vtkUnstructuredGrid::SafeDownCast(
+    outInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkSelection *sel = vtkSelection::SafeDownCast(
+    selInfo->Get(vtkDataObject::DATA_OBJECT()));
+  if ( ! sel )
+    {
+    vtkErrorMacro(<<"No selection specified");
+    return 1;
+    }
+
+  if (!sel->GetProperties()->Has(vtkSelection::CONTENT_TYPE()) ||
+      sel->GetProperties()->Get(vtkSelection::CONTENT_TYPE()) != vtkSelection::LOCATIONS)
+    {
+    return 1;
+    }
+
+  vtkDebugMacro(<< "Extracting from dataset");
+
+  //get a hold of input data structures and allocate output data structures
+  vtkDoubleArray *selLocations = 
+    vtkDoubleArray::SafeDownCast(sel->GetSelectionList());
+  vtkIdType numSLocations = selLocations->GetNumberOfTuples();
+
+  vtkIdType numILocations = input->GetNumberOfPoints();
+  vtkPointData *inPD = input->GetPointData();
+  vtkIdType numICells = input->GetNumberOfCells();
+  vtkCellData *inCD = input->GetCellData();
+
+  vtkPoints *newPts = vtkPoints::New();
+  newPts->Allocate(numSLocations*4);
+  output->Allocate(numSLocations);
+  vtkPointData *outputPD = output->GetPointData();
+  outputPD->CopyAllocate(inPD);
+  vtkCellData *outputCD = output->GetCellData();
+  outputCD->CopyAllocate(inCD);
+
+  vtkIdTypeArray *originalCellIds = vtkIdTypeArray::New();
+  outputCD->AddArray(originalCellIds);
+  originalCellIds->SetName("vtkOriginalCellIds");
+  originalCellIds->SetNumberOfComponents(1);
+
+  //find the cells in the input that contain the locations in the selection
+  //create a map to convert old to new point ids
+  vtkIdType *pointMap = new vtkIdType[numILocations]; 
+  vtkIdType ptId, newPointId;
+  for (ptId=0; ptId < numILocations; ptId++)
+    {
+    pointMap[ptId] = -1;
+    }
+
+  //information about of the current cell
+  vtkCell* cell;
+  vtkIdType numCellPts;
+  vtkIdList *cellPtIds;
+  vtkIdList *newCellPtIds = vtkIdList::New();
+  newCellPtIds->Allocate(VTK_CELL_SIZE);
+  double x[3];
+
+  //parameters for the is pt inside cell method
+  int inside;
+  double closestPoint[3];
+  int subId;
+  double pcoords[3];
+  double dist2;
+  double* weights = new double[input->GetMaxCellSize()];
+
+  for (vtkIdType c = 0; c < numICells; c++)
+    {
+    cell = input->GetCell(c);
+    int cellD = cell->GetCellDimension();
+
+    for (vtkIdType p = 0; p < numSLocations; p++)
+      {    
+      double *point = selLocations->GetTuple(p);
+
+      inside = cell->EvaluatePosition(
+        point,
+        closestPoint, subId, pcoords, dist2, weights);
+      
+      if (inside == 1 && dist2==0.0) //have to check dist for 2D cells
+        {
+        //copy over the locations that make up the cell
+        cellPtIds = cell->GetPointIds();
+        numCellPts = cell->GetNumberOfPoints();    
+        newCellPtIds->Reset();
+        //intersects, put all of the locations inside
+        for (vtkIdType i=0; i < numCellPts; i++)
+          {
+          ptId = cellPtIds->GetId(i);
+          newPointId = pointMap[ptId];
+          if (newPointId < 0)
+            {             
+            input->GetPoint(ptId, x);      
+            newPointId = newPts->InsertNextPoint(x);
+            outputPD->CopyData(inPD,ptId,newPointId);
+            pointMap[ptId] = newPointId;
+            }
+          newCellPtIds->InsertId(i,newPointId);
+          }
+        vtkIdType newCellId = 
+          output->InsertNextCell(cell->GetCellType(),newCellPtIds);
+        outputCD->CopyData(inCD,c,newCellId);
+        originalCellIds->InsertNextValue(c);
+        break;
+        }
+      }
+    }
+
+  output->SetPoints(newPts);
+  output->Squeeze();
+
+  delete[] pointMap;
+  newPts->Delete();  
+  newCellPtIds->Delete();
+  delete[] weights;  
+  originalCellIds->Delete();
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkExtractSelectedLocations::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+
+//----------------------------------------------------------------------------
+int vtkExtractSelectedLocations::FillInputPortInformation(
+  int port, vtkInformation* info)
+{
+  if (port==0)
+    {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");    
+    }
+  else
+    {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkSelection");
+    }
+  return 1;
+}
---
+++ VTK/Graphics/vtkExtractSelectedLocations.h
@@ -0,0 +1,60 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkExtractSelectedLocations.h,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkExtractSelectedLocations - extract cells within a dataset that 
+// contain the locations listen in the vtkSelection.
+// .SECTION Description
+// vtkExtractSelectedLocations extracts all cells whose volume contain at least 
+// one point listed in the LOCATIONS content of the vtkSelection. This filter 
+// adds a scalar array called vtkOriginalCellIds that says what input cell 
+// produced each output cell. This is an example of a Pedigree ID which helps
+// to trace back results.
+// .SECTION See Also
+// vtkSelection vtkExtractSelection
+
+#ifndef __vtkExtractSelectedLocations_h
+#define __vtkExtractSelectedLocations_h
+
+#include "vtkUnstructuredGridAlgorithm.h"
+
+class vtkSelection;
+
+class VTK_GRAPHICS_EXPORT vtkExtractSelectedLocations : public vtkUnstructuredGridAlgorithm
+{
+public:
+  vtkTypeRevisionMacro(vtkExtractSelectedLocations,vtkUnstructuredGridAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Construct object
+  static vtkExtractSelectedLocations *New();
+
+protected:
+  vtkExtractSelectedLocations();
+  ~vtkExtractSelectedLocations();
+
+  // Usual data generation method
+  int RequestData(vtkInformation *, 
+                  vtkInformationVector **, 
+                  vtkInformationVector *);
+
+
+  virtual int FillInputPortInformation(int port, vtkInformation* info);
+
+private:
+  vtkExtractSelectedLocations(const vtkExtractSelectedLocations&);  // Not implemented.
+  void operator=(const vtkExtractSelectedLocations&);  // Not implemented.
+};
+
+#endif
