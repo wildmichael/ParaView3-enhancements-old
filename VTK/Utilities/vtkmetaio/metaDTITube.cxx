fcc98a0754948bfbafb937ee55cdfcd73342c1a0
aylward <aylward>
2007-05-10 20:03:09 +0000

    COMP: Fixed warnings from BCC regarding inline functions (functions declared in headers)

--- VTK/Utilities/vtkmetaio/metaDTITube.cxx
+++ VTK/Utilities/vtkmetaio/metaDTITube.cxx
@@ -3,8 +3,8 @@
   Program:   MetaIO
   Module:    $RCSfile: metaDTITube.cxx,v $
   Language:  C++
-  Date:      $Date: 2007-05-10 19:17:20 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 2007-05-10 20:03:09 $
+  Version:   $Revision: 1.4 $
 
   Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
@@ -27,6 +27,67 @@
 #if (METAIO_USE_NAMESPACE)
 namespace METAIO_NAMESPACE {
 #endif
+
+DTITubePnt::
+DTITubePnt(int dim)
+{
+  m_Dim = dim;
+  m_X = new float[m_Dim];
+  m_TensorMatrix = new float[6];
+ 
+  unsigned int i=0;
+  for(i=0;i<m_Dim;i++)
+    {
+    m_X[i] = 0;
+    }
+  
+  // Initialize the tensor matrix to identity
+  for(i=0;i<6;i++)
+    {
+    m_TensorMatrix[i] = 0;
+    }
+  m_TensorMatrix[0] = 1;
+  m_TensorMatrix[3] = 1;
+  m_TensorMatrix[5] = 1;
+}
+
+DTITubePnt::
+~DTITubePnt()
+{
+  delete []m_X;
+  delete []m_TensorMatrix;
+  m_ExtraFields.clear();
+}
+
+const DTITubePnt::FieldListType & 
+DTITubePnt::
+GetExtraFields() const 
+{
+  return m_ExtraFields;
+}
+
+void DTITubePnt::
+AddField(const char* name, float value)
+{
+  FieldType field(name,value);
+  m_ExtraFields.push_back(field);
+}
+
+float DTITubePnt::
+GetField(const char* name) const
+{
+  FieldListType::const_iterator it = m_ExtraFields.begin();
+  while(it != m_ExtraFields.end())
+    {
+    if(!strcmp((*it).first.c_str(),name))
+      {
+      return (*it).second;
+      }
+    ++it;
+    }
+  return -1;
+}
+
 
 /** MetaDTITube Constructors */
 MetaDTITube::
