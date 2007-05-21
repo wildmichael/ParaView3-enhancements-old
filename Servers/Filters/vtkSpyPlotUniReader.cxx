d242caee63b69a869efb18c89a69b7d882e93cc1
henderson <henderson>
2007-05-21 19:47:54 +0000

    ENH: removing unused code

--- Servers/Filters/vtkSpyPlotUniReader.cxx
+++ Servers/Filters/vtkSpyPlotUniReader.cxx
@@ -11,7 +11,7 @@
 //=============================================================================
 //-----------------------------------------------------------------------------
 
-vtkCxxRevisionMacro(vtkSpyPlotUniReader, "$Revision: 1.5 $");
+vtkCxxRevisionMacro(vtkSpyPlotUniReader, "$Revision: 1.6 $");
 vtkStandardNewMacro(vtkSpyPlotUniReader);
 vtkCxxSetObjectMacro(vtkSpyPlotUniReader, CellArraySelection, vtkDataArraySelection);
 
@@ -802,6 +802,7 @@ int vtkSpyPlotUniReader::MakeCurrent()
   return 1;
 }
 
+#if 0
 //-----------------------------------------------------------------------------
 void vtkSpyPlotUniReader::PrintMemoryUsage()
 {
@@ -976,8 +977,8 @@ void vtkSpyPlotUniReader::PrintInformation()
     }
 
   this->CellArraySelection->Print(cout);
-
 }
+#endif
 
 //-----------------------------------------------------------------------------
 /* Routine run-length-encodes the data pointed to by *data, placing
