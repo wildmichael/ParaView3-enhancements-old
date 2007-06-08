0be382c9e99b577e5f1dd463e54612d0019c2b45
jeff <jeff>
2007-06-08 16:47:45 +0000

    ENH: Added PEDIGREEIDS selection type.  Updated vtkExtractSelection and vtkExtractSelectedIds to handle the new type.

--- VTK/Filtering/vtkSelection.cxx
+++ VTK/Filtering/vtkSelection.cxx
@@ -29,7 +29,7 @@
 #include <vtkstd/map>
 #include <vtkstd/vector>
 
-vtkCxxRevisionMacro(vtkSelection, "$Revision: 1.11 $");
+vtkCxxRevisionMacro(vtkSelection, "$Revision: 1.12 $");
 vtkStandardNewMacro(vtkSelection);
 
 vtkCxxSetObjectMacro(vtkSelection, SelectionList, vtkAbstractArray);
@@ -349,6 +349,7 @@ void vtkSelection::Union(vtkSelection* s)
   switch (type)
     {
     case GLOBALIDS:
+    case PEDIGREEIDS:
     case VALUES:
     case INDICES:
     case LOCATIONS:
