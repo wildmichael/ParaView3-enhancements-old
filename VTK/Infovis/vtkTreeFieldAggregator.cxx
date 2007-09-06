934ce901c5c98c50462265b679bdab2aa9751064
henderson <henderson>
2007-09-06 14:34:35 +0000

    BUG: LeafVertexUnitsSize and LogScale ivars should be initialized before calling Set* methods on them created with vtkSetMacro. Otherwise, the Set* methods are accessing uninitialized memory.

--- VTK/Infovis/vtkTreeFieldAggregator.cxx
+++ VTK/Infovis/vtkTreeFieldAggregator.cxx
@@ -35,15 +35,15 @@
 #include "vtkGraph.h"
 #include "vtkVariantArray.h"
 
-vtkCxxRevisionMacro(vtkTreeFieldAggregator, "$Revision: 1.6 $");
+vtkCxxRevisionMacro(vtkTreeFieldAggregator, "$Revision: 1.7 $");
 vtkStandardNewMacro(vtkTreeFieldAggregator);
 
 vtkTreeFieldAggregator::vtkTreeFieldAggregator():MinValue(0.0)
 {
   this->MinValue = 0;
   this->Field = 0;
-  this->SetLeafVertexUnitSize(true);
-  this->SetLogScale(false);
+  this->LeafVertexUnitSize = true;
+  this->LogScale = false;
 }
 
 vtkTreeFieldAggregator::~vtkTreeFieldAggregator()
