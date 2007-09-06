a9ceb549d8e1c62f4e17cabc33cfa455fc5b2250
henderson <henderson>
2007-09-06 14:35:08 +0000

    BUG: RectanglesFieldName ivar needs to be set to NULL in the destructor.

--- VTK/Infovis/vtkLabeledTreeMapDataMapper.cxx
+++ VTK/Infovis/vtkLabeledTreeMapDataMapper.cxx
@@ -36,7 +36,7 @@
 #include "vtkViewport.h"
 #include "vtkWindow.h"
 
-vtkCxxRevisionMacro(vtkLabeledTreeMapDataMapper, "$Revision: 1.11 $");
+vtkCxxRevisionMacro(vtkLabeledTreeMapDataMapper, "$Revision: 1.12 $");
 vtkStandardNewMacro(vtkLabeledTreeMapDataMapper);
 
 vtkLabeledTreeMapDataMapper::vtkLabeledTreeMapDataMapper()
@@ -115,7 +115,7 @@ vtkLabeledTreeMapDataMapper::~vtkLabeledTreeMapDataMapper()
     delete [] this->TextMappers;
     this->TextMappers = NULL;
     }
-  
+  this->SetRectanglesFieldName(NULL);
 }
 
 //----------------------------------------------------------------------------
