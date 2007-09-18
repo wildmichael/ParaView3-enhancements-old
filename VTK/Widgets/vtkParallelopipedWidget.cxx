e9840eb25f990e6441fbbfd174ee7d337b7848b6
henderson <henderson>
2007-09-18 19:41:29 +0000

    BUG: fixing memory leak

--- VTK/Widgets/vtkParallelopipedWidget.cxx
+++ VTK/Widgets/vtkParallelopipedWidget.cxx
@@ -32,7 +32,7 @@
 #include "vtkWidgetSet.h"
 #include "vtkGarbageCollector.h"
 
-vtkCxxRevisionMacro(vtkParallelopipedWidget, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkParallelopipedWidget, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkParallelopipedWidget);
 
 //----------------------------------------------------------------------
@@ -87,6 +87,7 @@ vtkParallelopipedWidget::~vtkParallelopipedWidget()
     {
     this->HandleWidgets[i]->Delete();
     }
+  delete [] this->HandleWidgets;
 }
 
 //----------------------------------------------------------------------
