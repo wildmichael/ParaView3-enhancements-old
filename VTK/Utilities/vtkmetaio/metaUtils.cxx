38d1dbf81e8e591d769dc16aa438c78bf79b1895
vtkrobot <vtkrobot>
2007-11-28 19:03:42 +0000

    r85 brad.king | 2007-11-28T19:00:47.606826Z STYLE: Removing trailing whitespace (testing ITK/VTK cvs tracking).

--- VTK/Utilities/vtkmetaio/metaUtils.cxx
+++ VTK/Utilities/vtkmetaio/metaUtils.cxx
@@ -3,8 +3,8 @@
   Program:   MetaIO
   Module:    $RCSfile: metaUtils.cxx,v $
   Language:  C++
-  Date:      $Date: 2007-11-28 19:02:52 $
-  Version:   $Revision: 1.14 $
+  Date:      $Date: 2007-11-28 19:03:42 $
+  Version:   $Revision: 1.15 $
 
   Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
