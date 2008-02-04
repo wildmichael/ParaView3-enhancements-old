9c78d7608d66ea20a74aa6028bc898eb2ffc61cb
jeff <jeff>
2008-02-04 18:33:39 +0000

    ENH: Changing name from vtkGraphToBoostAdapter.h to vtkBoostGraphAdapter.h

--- VTK/Infovis/vtkBoostGraphAdapter.h
+++ VTK/Infovis/vtkBoostGraphAdapter.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkGraphToBoostAdapter.h,v $
+  Module:    $RCSfile: vtkBoostGraphAdapter.h,v $
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
