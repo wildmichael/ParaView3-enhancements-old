e1aa4e578c8d0e762f32282e9b37e659c09a4927
dean <dean>
2008-04-16 16:04:15 +0000

    STYLE: minor typo

--- VTK/Widgets/vtkSeedRepresentation.cxx
+++ VTK/Widgets/vtkSeedRepresentation.cxx
@@ -26,12 +26,12 @@
 #include "vtkTextProperty.h"
 #include <vtkstd/list>
 
-vtkCxxRevisionMacro(vtkSeedRepresentation, "$Revision: 1.6 $");
+vtkCxxRevisionMacro(vtkSeedRepresentation, "$Revision: 1.7 $");
 vtkStandardNewMacro(vtkSeedRepresentation);
 
 vtkCxxSetObjectMacro(vtkSeedRepresentation,HandleRepresentation,vtkHandleRepresentation);
 
-// The vtkHandleList is a PIMPLed vector<T>.
+// The vtkHandleList is a PIMPLed list<T>.
 class vtkHandleList : public vtkstd::list<vtkHandleRepresentation*> {};
 typedef vtkstd::list<vtkHandleRepresentation*>::iterator vtkHandleListIterator;
 
