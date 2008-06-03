5d5f146ce338ccee160473e0d14c55727840a042
fbertel <fbertel>
2008-06-03 15:56:42 +0000

    BUG:No idea why my Linux box did not catch this one.

--- VTK/Imaging/vtkImageImport.cxx
+++ VTK/Imaging/vtkImageImport.cxx
@@ -27,7 +27,7 @@
 #include <ctype.h>
 #include <vtkstd/exception>
 
-vtkCxxRevisionMacro(vtkImageImport, "$Revision: 1.52 $");
+vtkCxxRevisionMacro(vtkImageImport, "$Revision: 1.53 $");
 vtkStandardNewMacro(vtkImageImport);
 
 
@@ -85,7 +85,8 @@ vtkImageImport::vtkImageImport()
   this->SetExecutive(exec);
   exec->Delete();
 
-  this->SetScalarArrayName("ImageFile");
+  this->ScalarArrayName=0;
+  this->SetScalarArrayName("");
 }
 
 //----------------------------------------------------------------------------
