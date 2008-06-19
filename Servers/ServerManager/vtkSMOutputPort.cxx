53ae9b6c74791ee215bed4b282f83acf5734cf83
utkarsh <utkarsh>
2008-06-19 16:41:48 +0000

    COMP: Fixed warnings.

--- Servers/ServerManager/vtkSMOutputPort.cxx
+++ Servers/ServerManager/vtkSMOutputPort.cxx
@@ -26,7 +26,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkSMOutputPort);
-vtkCxxRevisionMacro(vtkSMOutputPort, "$Revision: 1.6 $");
+vtkCxxRevisionMacro(vtkSMOutputPort, "$Revision: 1.7 $");
 
 
 //----------------------------------------------------------------------------
@@ -219,7 +219,7 @@ void vtkSMOutputPort::InvalidateDataInformation()
 
 //----------------------------------------------------------------------------
 // vtkPVPart used to update before gathering this information ...
-void vtkSMOutputPort::GatherDataInformation(int doUpdate)
+void vtkSMOutputPort::GatherDataInformation(int vtkNotUsed(doUpdate))
 {
   if (this->GetID().IsNull())
     {
