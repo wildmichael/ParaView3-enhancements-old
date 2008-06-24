36513b4466d3c46225f8fdc414e20578c4055a59
dcthomp <dcthomp>
2008-06-24 01:28:56 +0000

    COMP: Fixes from Matthew Wong.

--- VTK/IO/vtkPostgreSQLDatabase.cxx
+++ VTK/IO/vtkPostgreSQLDatabase.cxx
@@ -34,8 +34,10 @@
 
 #include <libpq-fe.h>
 
+#include <assert.h>
+
 vtkStandardNewMacro(vtkPostgreSQLDatabase);
-vtkCxxRevisionMacro(vtkPostgreSQLDatabase, "$Revision: 1.32 $");
+vtkCxxRevisionMacro(vtkPostgreSQLDatabase, "$Revision: 1.33 $");
 
 // ----------------------------------------------------------------------
 vtkPostgreSQLDatabase::vtkPostgreSQLDatabase()
