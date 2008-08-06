41146e901354c06450462419a26951d9eded4cd3
clarke <clarke>
2008-08-06 12:35:59 +0000

    Fixed warnings from CDash

--- Utilities/Xdmf2/vtk/vtkXdmfReader.cxx
+++ Utilities/Xdmf2/vtk/vtkXdmfReader.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkXdmfReader.cxx,v $
   Language:  C++
-  Date:      $Date: 2008-08-05 14:01:50 $
-  Version:   $Revision: 1.45 $
+  Date:      $Date: 2008-08-06 12:35:59 $
+  Version:   $Revision: 1.46 $
 
 
   Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen  
@@ -90,7 +90,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkXdmfReader);
-vtkCxxRevisionMacro(vtkXdmfReader, "$Revision: 1.45 $");
+vtkCxxRevisionMacro(vtkXdmfReader, "$Revision: 1.46 $");
 
 //----------------------------------------------------------------------------
 vtkCxxSetObjectMacro(vtkXdmfReader,Controller,vtkMultiProcessController);
@@ -113,8 +113,14 @@ class vtkXdmfReaderGrid
 public:
   //
   vtkXdmfReaderGrid() 
-    : XMGrid(0), DataDescription(0), Enabled(0), vtkType(-1), Time(-1), TimeIndex(0), isTemporal(0)
   {
+      this->XMGrid = 0;
+      this->DataDescription = 0;
+      this->Enabled = 0;
+      this->vtkType = -1;
+      this->Time = -1.0;
+      this->TimeIndex = 0;
+      this->isTemporal = 0;
   }
 
   ~vtkXdmfReaderGrid()
@@ -1797,13 +1803,13 @@ void vtkXdmfReader::AssignTimeIndex(vtkXdmfReaderGrid *ptr)
     {
     return;
     }
-    int ActualTimeStep = vtkstd::find_if(
+    int actualTimeStep = vtkstd::find_if(
           this->Internals->TimeValues.begin(), 
           this->Internals->TimeValues.end(), 
           vtkstd::bind2nd( vtkstd::greater_equal<double>( ), ptr->Time)) 
           - this->Internals->TimeValues.begin();
-    // cout << "ActualTimeStep for " << ptr->Time << " = " << ActualTimeStep << endl;
-    ptr->TimeIndex = ActualTimeStep;
+    // cout << "actualTimeStep for " << ptr->Time << " = " << actualTimeStep << endl;
+    ptr->TimeIndex = actualTimeStep;
   int T = ptr->Children.size();
   // cout << "FindTimeValues : ptr has " << T << " children" << endl;
   for (int i=0; i<T; i++) {
