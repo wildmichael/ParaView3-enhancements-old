b8951b399bfcaaf43ad54e181c5aa10360f9bc62
pppebay <pppebay>
2008-10-31 21:28:32 +0000

    COMP: added missing iVar to PrintSelf()

--- VTK/Infovis/vtkStatisticsAlgorithm.cxx
+++ VTK/Infovis/vtkStatisticsAlgorithm.cxx
@@ -28,7 +28,7 @@
 #include "vtkStringArray.h"
 #include "vtkTable.h"
 
-vtkCxxRevisionMacro(vtkStatisticsAlgorithm, "$Revision: 1.16 $");
+vtkCxxRevisionMacro(vtkStatisticsAlgorithm, "$Revision: 1.17 $");
 
 // ----------------------------------------------------------------------
 vtkStatisticsAlgorithm::vtkStatisticsAlgorithm()
@@ -58,6 +58,7 @@ vtkStatisticsAlgorithm::~vtkStatisticsAlgorithm()
 void vtkStatisticsAlgorithm::PrintSelf( ostream &os, vtkIndent indent )
 {
   this->Superclass::PrintSelf( os, indent );
+  os << indent << "NumberOfVariables: " << this->NumberOfVariables << endl;
   os << indent << "SampleSize: " << this->SampleSize << endl;
   os << indent << "Learn: " << this->Learn << endl;
   os << indent << "Derive: " << this->Derive << endl;
