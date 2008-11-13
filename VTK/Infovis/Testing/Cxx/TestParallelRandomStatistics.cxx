99ea63a79f441a6238a0bb3a5ea58a427abc15c7
pppebay <pppebay>
2008-11-13 23:50:07 +0000

    STYLE: a more appropriate test name, since it is not limited to parallel        descriptive statistics.

--- VTK/Infovis/Testing/Cxx/TestParallelRandomStatistics.cxx
+++ VTK/Infovis/Testing/Cxx/TestParallelRandomStatistics.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: TestPDescriptiveStatistics.cxx,v $
+  Module:    $RCSfile: TestParallelRandomStatistics.cxx,v $
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
