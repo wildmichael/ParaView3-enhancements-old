205551ab78f97e359c8ea2637ba10563b9cb428c
dave.demarle <dave.demarle>
2008-12-19 17:26:57 +0000

    ENH: Fix streaming options

--- Plugins/Streaming/Server/vtkSMStreamingRepresentation.cxx
+++ Plugins/Streaming/Server/vtkSMStreamingRepresentation.cxx
@@ -36,7 +36,7 @@
 #include "vtkSMSUnstructuredGridParallelStrategy.h"
 #include "vtkStreamingOptions.h"
 
-vtkCxxRevisionMacro(vtkSMStreamingRepresentation, "$Revision: 1.4 $");
+vtkCxxRevisionMacro(vtkSMStreamingRepresentation, "$Revision: 1.5 $");
 vtkStandardNewMacro(vtkSMStreamingRepresentation);
 
 #define DEBUGPRINT_REPRESENTATION(arg)\
@@ -328,7 +328,7 @@ bool vtkSMStreamingRepresentation::AddToView(vtkSMViewProxy* view)
     return false;
     }
 
-  this->PieceBoundsRepresentation->AddToView(streamView->GetRootView());
+  //this->PieceBoundsRepresentation->AddToView(streamView->GetRootView());
 
   //this tells renderview to let view create strategy
   vtkSMRenderViewProxy* renderView = streamView->GetRootView();
