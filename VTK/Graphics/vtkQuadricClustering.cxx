f1357a4cad0ea7aec44269442e6e550d13de4941
fbertel <fbertel>
2009-02-06 18:47:19 +0000

    BUG:Fixed Mismatching allocation and deallocation, found by static code analyzer cppcheck.

--- VTK/Graphics/vtkQuadricClustering.cxx
+++ VTK/Graphics/vtkQuadricClustering.cxx
@@ -28,7 +28,7 @@
 #include "vtkTriangle.h"
 #include <vtkstd/set> // keep track of inserted triangles
 
-vtkCxxRevisionMacro(vtkQuadricClustering, "$Revision: 1.87 $");
+vtkCxxRevisionMacro(vtkQuadricClustering, "$Revision: 1.88 $");
 vtkStandardNewMacro(vtkQuadricClustering);
 
 //----------------------------------------------------------------------------
@@ -1393,7 +1393,7 @@ void vtkQuadricClustering::EndAppendVertexGeometry(vtkPolyData *input,
       {
       if (tmp)
         {
-        delete tmp;
+        delete[] tmp;
         }
       tmp = new vtkIdType[numPts];
       tmpLength = numPts;
