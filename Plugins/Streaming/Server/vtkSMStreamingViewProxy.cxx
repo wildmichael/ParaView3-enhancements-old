2894dac57cee077640efce8b65726198980f37f4
dave.demarle <dave.demarle>
2009-03-09 17:42:55 +0000

    ENH: Fix two annoyances in streaming paraview. First render in streaming view no longer shows garbage, and you get choice of arrays to color by without having to make the data visible first

--- Plugins/Streaming/Server/vtkSMStreamingViewProxy.cxx
+++ Plugins/Streaming/Server/vtkSMStreamingViewProxy.cxx
@@ -45,7 +45,7 @@
 #include <vtksys/ios/sstream>
 
 //-----------------------------------------------------------------------------
-vtkCxxRevisionMacro(vtkSMStreamingViewProxy, "$Revision: 1.8 $");
+vtkCxxRevisionMacro(vtkSMStreamingViewProxy, "$Revision: 1.9 $");
 vtkStandardNewMacro(vtkSMStreamingViewProxy);
 
 #define DEBUGPRINT_VIEW(arg)\
@@ -461,12 +461,10 @@ void vtkSMStreamingViewProxy::PrepareRenderPass()
     //cls
     if (firstpass) //workaround a crash that shows up on some mac's
       {
+      renWin->Render();
       firstpass = false;
       }
-    else
-      {
-      ren->Clear(); 
-      }    
+    ren->Clear(); 
     //don't cls on following render passes
     renWin->EraseOff();
     ren->EraseOff();
