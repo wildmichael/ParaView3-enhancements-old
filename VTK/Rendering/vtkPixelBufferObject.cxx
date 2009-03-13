85e8aa3e6a4d1793fc1590dafa1ece6a3f91246e
fbertel <fbertel>
2009-03-13 14:03:22 +0000

    BUG:Fixed uninitialized variables. Detected by valgrind on otherPrint test.

--- VTK/Rendering/vtkPixelBufferObject.cxx
+++ VTK/Rendering/vtkPixelBufferObject.cxx
@@ -29,13 +29,15 @@
 #endif
 
 vtkStandardNewMacro(vtkPixelBufferObject);
-vtkCxxRevisionMacro(vtkPixelBufferObject, "$Revision: 1.6 $");
+vtkCxxRevisionMacro(vtkPixelBufferObject, "$Revision: 1.7 $");
 //----------------------------------------------------------------------------
 vtkPixelBufferObject::vtkPixelBufferObject()
 {
   this->Handle = 0;
   this->Context = 0;
   this->BufferTarget = 0;
+  this->Size=0;
+  this->Type=VTK_UNSIGNED_CHAR;
 }
 
 //----------------------------------------------------------------------------
