f3ae605b8a69536e60d6edd4c238dc0a5cb158e0
tshead <tshead>
2009-07-21 18:59:44 +0000

    ENH: Provide functionality for matching Mime types.

--- VTK/TextAnalysis/vtkFoldCase.cxx
+++ VTK/TextAnalysis/vtkFoldCase.cxx
@@ -29,11 +29,9 @@
 #include <vtkTable.h>
 #include <vtkUnicodeStringArray.h>
 
-#include <boost/algorithm/string.hpp>
-
 #include <stdexcept>
 
-vtkCxxRevisionMacro(vtkFoldCase, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkFoldCase, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkFoldCase);
 
 vtkFoldCase::vtkFoldCase() :
