bfae2b93ee314b16a01583fc4bc88032782f2096
partyd <partyd>
2009-09-01 19:27:02 +0000

    BUG: Fix QtXMLProcessor to properly handle 64bit Ids by converting all numerics to double.

--- VTK/TextAnalysis/vtkQtXMLProcessor.cxx
+++ VTK/TextAnalysis/vtkQtXMLProcessor.cxx
@@ -223,6 +223,9 @@ public:
         if(value.IsInt())
           return value.ToInt();
 
+        if(value.IsNumeric())
+          return value.ToDouble();
+
         if(value.IsString())
           return value.ToString().c_str();
 
@@ -299,7 +302,7 @@ public:
 ////////////////////////////////////////////////////////////////////////////////////////
 // vtkQtXMLProcessor
 
-vtkCxxRevisionMacro(vtkQtXMLProcessor, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkQtXMLProcessor, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkQtXMLProcessor);
 
 vtkQtXMLProcessor::vtkQtXMLProcessor() :
