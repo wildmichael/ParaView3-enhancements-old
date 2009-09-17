cf8fea4bf62a0f50683bf6b093d641010cd362a3
zhanping.liu <zhanping.liu>
2009-09-17 22:44:09 +0000

    BUG: removed registering each dataset to the interpolator and added 'virtual' to destructors.

--- VTK/Filtering/vtkInterpolatedVelocityField.cxx
+++ VTK/Filtering/vtkInterpolatedVelocityField.cxx
@@ -19,7 +19,7 @@
 #include "vtkObjectFactory.h"
 
 //----------------------------------------------------------------------------
-vtkCxxRevisionMacro( vtkInterpolatedVelocityField, "$Revision: 1.11 $" );
+vtkCxxRevisionMacro( vtkInterpolatedVelocityField, "$Revision: 1.12 $" );
 vtkStandardNewMacro( vtkInterpolatedVelocityField ); 
 
 //----------------------------------------------------------------------------
@@ -31,7 +31,7 @@ void vtkInterpolatedVelocityField::AddDataSet( vtkDataSet * dataset )
     }
 
   this->DataSets->push_back( dataset );
-  dataset->Register( this ); // register this dataset
+  //dataset->Register( this );
 
   int size = dataset->GetMaxCellSize();
   if ( size > this->WeightsSize )
